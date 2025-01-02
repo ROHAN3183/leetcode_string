a55f79b4f80f50055610eec3057d04a358ed43d5
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-01-02 20:07:21 +0530

    Update 2559-count-vowel-strings-in-ranges.java

--- 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.java
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.java
@@ -1,3 +1,29 @@
+#01 BRUTE 
+class Solution {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int n=queries.length;
+        int [] ans=new int [n];
+        for(int i=0;i<n;i++){
+            int start=queries[i][0];
+            int end=queries[i][1];
+            int count=0;
+            for(int j=start;j<=end;j++){
+                char first=words[j].charAt(0);
+                char last=words[j].charAt(words[j].length()-1);
+                if(isVowel(first)&&isVowel(last)){
+                    count++;
+                }
+            }
+            ans[i]=count;
+        }
+        return ans;
+    }
+    boolean isVowel(char c){
+        return "aeiou".indexOf(c)!=-1;
+    }
+}
+
+#2 OPTIMIZED 
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
         int n=queries.length;

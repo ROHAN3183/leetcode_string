fbe85485112a4fe00f689acb60151fe2479eab5b
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-01-03 21:50:12 +0530

    Time: 3 ms (59.69%), Space: 61.2 MB (14.07%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,28 +1,4 @@
-#1 Brute Force  
 class Solution {
-    public int waysToSplitArray(int[] nums) {
-        int n=nums.length;
-        int count=0;
-        for(int i=0;i<n-1;i++){
-            long left_sum=0;
-            long right_sum=0;
-            for(int j=0;j<=i;j++){
-                left_sum=left_sum+nums[j];
-            }
-            for(int k=i+1;k<n;k++){
-                right_sum=right_sum+nums[k];
-            }
-            if(left_sum >=right_sum){
-                count++;
-            }
-        }
-        return count;
-    }
-}
-
-#2 optimized 
-    
-    class Solution {
     public int waysToSplitArray(int[] nums) {
         int n=nums.length;
         int j=0;

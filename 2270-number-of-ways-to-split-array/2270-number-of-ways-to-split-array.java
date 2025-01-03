e0ca2cf7f8e5ea738a2475bea0b4b057e2a56a10
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-01-03 21:33:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,21 +1,19 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
         int n=nums.length;
-        int j=0;
         int count=0;
-        long []prefix_sum=new long[n];
-        long sum=0;
-
-        for(int i=0;i<n;i++){
-            sum=sum+nums[i];
-            prefix_sum[i]=sum;
-        }
-
-        while(j<=n-2){
-            if(prefix_sum[j]>=prefix_sum[n-1]-prefix_sum[j]){
+        for(int i=0;i<n-1;i++){
+            long left_sum=0;
+            long right_sum=0;
+            for(int j=0;j<=i;j++){
+                left_sum=left_sum+nums[j];
+            }
+            for(int k=i+1;k<n;k++){
+                right_sum=right_sum+nums[k];
+            }
+            if(left_sum >=right_sum){
                 count++;
             }
-            j++;
         }
         return count;
     }

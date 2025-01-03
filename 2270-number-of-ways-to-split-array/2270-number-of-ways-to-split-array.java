8238502eb623e174e1ddae09ffef0d3c4da684ce
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-01-03 21:52:45 +0530

    Update 2270-number-of-ways-to-split-array.java

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,3 +1,4 @@
+#optimized
 class Solution {
     public int waysToSplitArray(int[] nums) {
         int n=nums.length;
@@ -20,3 +21,26 @@ class Solution {
         return count;
     }
 }
+
+#brute 
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        for(int i=0;i<n-1;i++){
+            long left_sum=0;
+            long right_sum=0;
+            for(int j=0;j<=i;j++){
+                left_sum=left_sum+nums[j];
+            }
+            for(int k=i+1;k<n;k++){
+                right_sum=right_sum+nums[k];
+            }
+            if(left_sum >=right_sum){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+    

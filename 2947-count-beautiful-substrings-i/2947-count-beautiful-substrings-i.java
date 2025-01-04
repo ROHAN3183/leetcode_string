a167acf1f2fa67e55a1ad75498a3980034170093
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-01-05 00:29:34 +0530

    Time: 179 ms (20.5%), Space: 42.7 MB (32.3%) - LeetHub

--- 2947-count-beautiful-substrings-i/2947-count-beautiful-substrings-i.java
+++ 2947-count-beautiful-substrings-i/2947-count-beautiful-substrings-i.java
@@ -1,36 +1,29 @@
 class Solution {
     public int beautifulSubstrings(String s, int k) {
-        int n=s.length();
-        int i=0;
-        int j=0;
-        int vowel_count=0;
-        int consonant_count=0;
-        int result=0;
-        while(i<n){
-           if(j<n){
-             char ch =s.charAt(j);
-            if(isVowel(ch)){
-                vowel_count++;
-            }
-            else{
-                consonant_count++;
-            }
-            if(vowel_count==consonant_count && (vowel_count*consonant_count)%k==0){
-                result++;
-            }
-            j++;
+        int n = s.length();
+        int result = 0;
 
-           }
-            else{
-                    vowel_count=0;
-                    consonant_count=0;
-                    i++;
-                    j=i;
+        for (int i = 0; i < n; i++) {
+            int vowel_count = 0;
+            int consonant_count = 0;
+
+            for (int j = i; j < n; j++) {
+                char ch = s.charAt(j);
+                if (isVowel(ch)) {
+                    vowel_count++;
+                } else {
+                    consonant_count++;
+                }
+
+                if (vowel_count == consonant_count && (vowel_count * consonant_count) % k == 0) {
+                    result++;
+                }
             }
         }
         return result;
     }
-    boolean isVowel(char ch){
-        return "aeiou".indexOf(ch)!=-1;
+
+    boolean isVowel(char ch) {
+        return "aeiou".indexOf(ch) != -1;
     }
 }

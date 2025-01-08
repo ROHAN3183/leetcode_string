9f21fcbec8b875b372c49e61b87f0a0bdf7d3ec3
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-01-08 11:47:01 -0800

    Time: 4 ms (23.9%), Space: 44.9 MB (5.54%) - LeetHub

---
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countPrefixSuffixPairs(String[] words) {
+        int n=words.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            String s=words[i];
+            for(int j=i+1;j<n;j++){
+                String str=words[j];
+                if(isValid(s,str)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    boolean isValid(String s,String str){
+        int n1=s.length();
+        int n2=str.length();
+        if(n1>n2){
+            return false;
+        }
+        String prefix=str.substring(0,n1);
+        String suffix=str.substring(n2-n1);
+        if(s.equals(prefix) && s.equals(suffix)){
+            return true;
+        }
+        return false;
+    }
+}

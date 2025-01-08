dcc69094984d3a5c314a0934fbb8f29f461823b0
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-01-08 23:40:26 +0530

    Update 1408-string-matching-in-an-array.java

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -1,3 +1,31 @@
+#BRUTE FORCE
+    
+    class Solution {
+    public List<String> stringMatching(String[] words) {
+        int n=words.length;
+        ArrayList<String>list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            String x=words[i];
+            for(int j=0;j<n;j++){
+                String str=words[j];
+                if(i!=j){
+                    if(isValid(str,x)){
+                        if(!list.contains(str)){
+                            list.add(str);
+                        }
+                    }
+                }
+            }
+        }
+        return list;
+    }
+    boolean isValid(String str,String x){
+        return (x).indexOf(str)!=-1;
+    }
+}
+
+# OPTIMIZED 
+    
 class Solution {
     public List<String> stringMatching(String[] words) {
         int n = words.length;

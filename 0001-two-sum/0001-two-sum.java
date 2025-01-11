e7121f2856b29cff603be25e1f4a610c369f8062
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-01-11 23:25:00 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,3 +1,19 @@
+#BRUTE 
+    class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) { // Check every pair
+                if (nums[i] + nums[j] == target) { // If pair sum equals target
+                    return new int[] {i, j}; // Return the indices
+                }
+            }
+        }
+        return new int[0]; // Return empty array if no solution
+    }
+}
+
+#OPTIMIZED
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer>map=new HashMap<>();

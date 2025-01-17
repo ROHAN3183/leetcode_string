1de9e352207217dfd016d0b261a935ba39f82de5
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-01-16 21:31:02 -0800

    Time: 0 ms (100%), Space: 40.4 MB (73.04%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int res=start^goal;
+        int count=0;
+        for(int i=0;i<31;i++){
+            if((res & (1<<i))!=0){
+                count++;
+            }
+        }
+        return count;
+    }
+}

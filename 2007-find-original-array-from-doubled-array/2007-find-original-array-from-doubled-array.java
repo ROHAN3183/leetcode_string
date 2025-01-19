51412d647c6102956c1ec556c4781d48fb6b8202
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-01-19 01:38:54 -0800

    Update 2007-find-original-array-from-doubled-array.java

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -6,17 +6,17 @@ class Solution {
         }
         HashMap<Integer,Integer>map=new HashMap<>();
         int [] result=new int[n/2];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){    //To fill the frequency of the changed;
             map.put(changed[i],map.getOrDefault(changed[i],0)+1);
         }
         int idx=0;
         Arrays.sort(changed);
         for(int i=0;i<n;i++){
            int nums=changed[i];
-            if(map.get(nums)==0){
+            if(map.get(nums)==0){ //if already used means frequency has became zero. then move for next iteration;
                 continue;
             }
-            if(map.getOrDefault(2*nums,0)>0){
+            if(map.getOrDefault(2*nums,0)>0){ //if  double of frequency is still there means has not used; 
                 result[idx++]=nums;
                 map.put(nums,map.get(nums)-1);
                 map.put(2*nums,map.get(2*nums)-1);

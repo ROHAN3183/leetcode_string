99761953e05aded1777afd762057bc637e802486
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-01-21 07:41:48 -0800

    Time: 14 ms (14.29%), Space: 47.6 MB (21.52%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,5 +1,42 @@
+
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
         
+        // Map to store diagonals
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        
+        // Fill the map using (i + j) as the key
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // If the map doesn't contain the diagonal index, initialize it
+                if (!map.containsKey(i + j)) {
+                    map.put(i + j, new ArrayList<>());
+                }
+                map.get(i + j).add(mat[i][j]);
+            }
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        boolean flip = true;
+        
+        // Traverse the map and fill the result list
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            List<Integer> diagonal = entry.getValue();
+            if (flip) {
+                Collections.reverse(diagonal);  // Reverse the diagonal list
+            }
+            result.addAll(diagonal);
+            flip = !flip;
+        }
+        
+        // Convert List<Integer> to int[] array
+        int[] resultArray = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            resultArray[i] = result.get(i);
+        }
+        
+        return resultArray;
     }
 }

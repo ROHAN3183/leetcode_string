bde290dd456defe8baa789a7e77e0d5b646c7b59
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-03 19:54:47 +0530

    Update 0876-middle-of-the-linked-list.java

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -8,8 +8,28 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+#BRUTE CODE
 class Solution {
     public ListNode middleNode(ListNode head) {
+        ListNode temp=head;
+        ListNode traverse=head;
+        int count =0;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        int result=0;
+        while(result<count/2){
+            result++;
+            traverse=traverse.next;
+        }
+        head=traverse;
+        return head;
+    }
+}
+# OPTIMIZED CODE 
+class Solution {
+    public ListNode middleNode(ListNode head) { // FAST AND SLOW Approach
         ListNode temp=head;
         ListNode traverse=head;
         int count =0;

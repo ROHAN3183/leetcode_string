47bace2604a04900197a6d4527bd545fa8783470
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-03 06:29:57 -0800

    Time: 0 ms (100%), Space: 41.3 MB (43.76%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -8,28 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-#BRUTE CODE
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode temp=head;
-        ListNode traverse=head;
-        int count =0;
-        while(temp!=null){
-            count++;
-            temp=temp.next;
-        }
-        int result=0;
-        while(result<count/2){
-            result++;
-            traverse=traverse.next;
-        }
-        head=traverse;
-        return head;
-    }
-}
-# OPTIMIZED CODE 
-class Solution {
-    public ListNode middleNode(ListNode head) { // slow and fast approach
         ListNode slow=head;
         ListNode fast =head;
         while(fast!=null && fast.next!=null){

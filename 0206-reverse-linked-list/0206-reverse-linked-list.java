15bd04356247fe0fbf708d1f6b32ea33204f3428
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-05 23:00:49 +0530

    Update 0206-reverse-linked-list.java

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,9 +8,10 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+# BRUTE FORCE
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        Stack<Integer>stack=new Stack<>();
+    public ListNode reverseList(ListNode head) { // Using extra space adding to stack then again poping back to linklist because 
+        Stack<Integer>stack=new Stack<>();      // stack follow LIFO .
         ListNode temp=head;
         while(temp!=null){
             stack.push(temp.val);
@@ -23,5 +24,23 @@ class Solution {
         }
 
         return head;
+    }
+}
+# OPTIMIZED CODE
+class Solution {
+    public ListNode reverseList(ListNode head) {// Placing last node in current node.next
+        if(head==null ||head.next==null){
+            return head;
+        }
+        ListNode prev=null;
+        ListNode temp=head;
+        ListNode x=null;
+        while(temp!=null){
+            x=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=x;
+        }
+        return prev;
     }
 }

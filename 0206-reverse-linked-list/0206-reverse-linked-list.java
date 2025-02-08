50887d0b95e3393f913a60e06264dcf550f979cb
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-08 02:04:09 -0800

    Time: 0 ms (100%), Space: 42.2 MB (96.38%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,27 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-# BRUTE FORCE
 class Solution {
-    public ListNode reverseList(ListNode head) { // Using extra space adding to stack then again poping back to linklist because 
-        Stack<Integer>stack=new Stack<>();      // stack follow LIFO .
-        ListNode temp=head;
-        while(temp!=null){
-            stack.push(temp.val);
-            temp=temp.next;
-        }
-        temp=head;
-        while(temp!=null){
-            temp.val=stack.pop();
-            temp=temp.next;
-        }
-
-        return head;
-    }
-}
-# OPTIMIZED CODE
-class Solution {
-    public ListNode reverseList(ListNode head) {// Placing last node in current node.next
+    public ListNode reverseList(ListNode head) {
         if(head==null ||head.next==null){
             return head;
         }

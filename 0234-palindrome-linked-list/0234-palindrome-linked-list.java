5ed0895bbc586da689086fb1a2a9719f6deea42e
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-08 03:00:48 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,19 +11,35 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode temp=head;
-        Stack<Integer> stack=new Stack<>();
-        while(temp!=null){
-            stack.push(temp.val);
-            temp=temp.next;
+        ListNode fast=head;
+        ListNode slow=head;
+        ListNode prev=null;
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        temp=head;
-        while(temp!=null&&!stack.isEmpty()){
-            int reversed_character=stack.pop();
-            if(reversed_character!=temp.val){
+        prev.next=null;
+        prev=reversed(slow);
+        while(temp!=null && prev!=null){
+            if(temp.val!=prev.val){
                 return false;
             }
             temp=temp.next;
+            prev=prev.next;
         }
         return true;
     }
+    private ListNode reversed(ListNode slow){
+        ListNode current=slow;
+        ListNode prev=null;
+        while(current!=null){
+            ListNode nextnode=current.next;
+            current.next=prev;
+            prev=current;
+            current=nextnode;
+        }
+        return prev;
+    }
 }
+

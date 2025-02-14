d8a68e94c53d9f768b9b0ed2b46e7a3bf02a3b73
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-14 09:23:23 -0800

    Time: 2 ms (99.05%), Space: 44.3 MB (36.82%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,4 +1,3 @@
-#BRUTE FORCE
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n=nums1.length;
@@ -20,36 +19,6 @@ class Solution {
                     result[i]=nums2[j];
                     break;
                 }
-            }
-        }
-        return result;
-    }
-}
-#OPTIMIZED CODE
-class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer>stack=new Stack<>();
-        HashMap<Integer,Integer>map=new HashMap<>();
-        int n=nums1.length;
-        int m=nums2.length;
-        int []result=new int[n];
-        int j=m-1;
-        while(j>=0){
-             while(!stack.isEmpty()&&stack.peek()<=nums2[j]){
-                    stack.pop();
-                }
-            if(stack.isEmpty()){
-                map.put(nums2[j],-1);
-            }
-            else{
-                map.put(nums2[j],stack.peek());
-            }
-            stack.push(nums2[j]);
-            j--;
-        }
-        for(int i=0;i<n;i++){
-            if(map.containsKey(nums1[i])){
-              result[i]=map.get(nums1[i]);
             }
         }
         return result;

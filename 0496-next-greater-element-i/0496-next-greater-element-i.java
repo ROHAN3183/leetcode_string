c43b3c9a765005c8f743e73a228653ab3b3c1481
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-14 22:52:20 +0530

    Update 0496-next-greater-element-i.java

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,3 +1,31 @@
+#BRUTE FORCE
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int m=nums2.length;
+        int idx=0;
+        int []result=new int[n];
+        Arrays.fill(result,-1);
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<m;i++){
+            map.put(nums2[i],i);
+        }
+        for(int i=0;i<n;i++){
+            int element=nums1[i];
+            if(map.containsKey(element)){
+                idx=map.get(element);
+            }
+            for(int j=idx+1;j<m;j++){
+                if(nums2[j]>element){
+                    result[i]=nums2[j];
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
+#OPTIMIZED CODE
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer>stack=new Stack<>();

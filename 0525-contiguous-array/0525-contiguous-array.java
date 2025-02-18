9580880fa595c93a3608cdf8e6a5fa64d7541c4a
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-18 19:23:56 +0530

    Update 0525-contiguous-array.java

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,3 +1,33 @@
+                                LOGIC OF THIS CODE
+1) USE HASHMAP TO STORE THE PREFIX SUM SUCH THAT TREAT O AS THE -1;
+2) CHECK THE MAP IF IT CONTAIN KEY ALREADY THEN GET THE VALUE AS IT WILL BE THE INDEX JUST END WILL BE YOUR CURRENT INDEX WHERE AS 
+    START WILL BE THE LAST SEEN INDEX STORED IN THE MAP.
+    
+# BRUTE FORCE
+    class Solution {
+    public int findMaxLength(int[] nums) {
+        int n=nums.length;
+
+        int max_length=0;
+        for(int i=0;i<n;i++){
+            int count_one=0;
+            for(int j=i;j<n;j++){
+                if(nums[j]==1){
+                    count_one++;
+                }
+                else{
+                    count_one--;
+                }
+                if(count_one==0){
+                    max_length=Math.max(j-i+1,max_length);
+                }
+            }
+        }
+        return max_length;
+    }
+}
+
+# OPTIMIZED CODE
 class Solution {
     public int findMaxLength(int[] nums) {
         HashMap<Integer,Integer>map=new HashMap<>();

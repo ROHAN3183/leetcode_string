517f0fced8d163bfff809e418fb38b890383beac
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-20 02:17:18 -0800

    Time: 2 ms (97.69%), Space: 41.8 MB (65.34%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,27 +1,23 @@
-                                                Logic Of Code
-1)if opening symbol means (,{,] then push it into the stack but if closing sysmbol is there then match it with stack peek 
-2)if stack peek is  opposite of the same symbol then pop out else return false.
-                                                    
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack=new Stack<>();
         int n=s.length();
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
-            if(ch=='('||ch=='{'||ch=='['){
-                stack.push(ch);
+            if(ch=='('){
+                stack.push(')');
+            }
+            else if(ch=='{'){
+                stack.push('}');
+            }
+            else if(ch=='['){
+                stack.push(']');
             }
             else{
-                if(stack.isEmpty()){
+                if(stack.isEmpty()||stack.peek()!=ch){
                     return false;
                 }
-                if(stack.peek()=='('&&ch==')'||stack.peek()=='{' && ch=='}'||
-                stack.peek()=='['&&ch==']'){
-                    stack.pop();
-                }
-                else{
-                    return false;
-                }
+                stack.pop();
             }
         }
         if(!stack.isEmpty()){

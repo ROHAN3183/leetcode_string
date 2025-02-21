aed4716e87f9342afdc565ca9c77ecee4dad4ebd
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-21 18:09:39 +0530

    Update 0155-min-stack.java

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,4 +1,47 @@
-class MinStack {
+                                Logic of code using space (2n) //for this follow apna college
+1) In this take a stack which will store two pair first will be the val whereas the second will store the value of the minimum 
+    as so far seen in the stack .
+2) if coming value in the stack is smaller then the peek of the top of the second then now put the new value in the first part 
+    and in the second part will be the min of new comming value vs already peek second value.
+    
+#1 class MinStack {
+    private Stack<int[]> stack=new Stack<>();
+    public MinStack() {
+        stack=new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(stack.isEmpty()){
+            stack.push(new int[]{val,val});
+        }
+        else{
+            stack.push(new int[]{val,Math.min(val,stack.peek()[1])});
+        }
+    }
+    
+    public void pop() {
+        if(stack.isEmpty()){
+            return;
+        }
+        else{
+            stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+                                        Logic of this code using space(1)
+Alway remember this formula that is (val'=2*val-min_val) this will come under the use whenever the comming val is smaller then the
+min_val.otherwise just push into the stack .while during the poping out the stack.peek() if stack.peek()<min_val then we need to 
+chnge the min_val also because it will get deleted for that formula is (min_val=2*min_val-stack.peek).
+
+#2 class MinStack {
     private Stack<Long> stack=new Stack<>();
     private long min_val; 
     public MinStack() {
@@ -46,12 +89,3 @@ class MinStack {
         return (int)min_val;
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */

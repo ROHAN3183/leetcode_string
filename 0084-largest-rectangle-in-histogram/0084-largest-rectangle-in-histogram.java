ff79ac41a8f68c27fbda8c070bee00cd2f53c6a8
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-21 11:32:31 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n=heights.length;
+        int max_area=Integer.MIN_VALUE;
+        int[] NSR=nearestSmallerToRight(heights,n);
+        int[] NSL=nearestSmallerToLeft(heights,n);
+        for(int i=0;i<n;i++){
+           int width=NSR[i]-NSL[i]-1;
+           int area=width*heights[i];
+           max_area=Math.max(area,max_area);
+        }
+        return max_area;
+    }
+    private int[] nearestSmallerToLeft(int []heights,int n){
+        Stack<Integer> stack=new Stack<>();
+        int [] NSL= new int[n];
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && heights[stack.peek()]>=heights[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                NSL[i]=n;
+            }
+            else{
+                NSL[i]=stack.peek();
+            }
+            stack.push(i);
+        }
+        return NSL;
+    }
+    private int[] nearestSmallerToRight(int []heights,int n){
+        Stack<Integer> stack=new Stack<>();
+        int [] NSR= new int[n];
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() &&  heights[stack.peek()]>=heights[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                NSR[i]=n;
+            }
+            else{
+                NSR[i]=stack.peek();
+            }
+            stack.push(i);
+        }
+        return NSR;
+    }
+}

d9b200058639b89fc2296aef1bbdc46f19c5c07f
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-24 12:12:59 +0530

    Update 0402-remove-k-digits.java

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,3 +1,8 @@
+logic of the code:
+Always increasing number is smaller ex-1234 is smaller then 1243 so use the concept of the monostack means alway push the digit
+which is greater then the previous oone or peek() . else keep deleting it also remember the delete the digit from the stack only
+k time.if after pushing in the stack also k didn't got 0 means need to  delete the last digit from the k stack because it is greater.
+    
 class Solution {
     public String removeKdigits(String num, int k) {
         Stack<Character> stack=new Stack<>();

d8d15dd2676e550a937ced77d2b4412c3aba49fb
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-02-26 22:21:43 +0530

    Update 0456-132-pattern.java

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -1,3 +1,8 @@
+logic for the code 
+take num1<num2<num3. 
+It means num3 is alway largest so .if in stack smaller element is comming the push it into the stack
+otherwise pop() put means because poped out element can become the num2 pop out till the comming element is smaller than the peek()
+the last smallest element will become the num2 .if comming element is smaller than the num2 means it is valid  
 class Solution {
     public boolean find132pattern(int[] nums) {
         Stack<Integer> stack=new Stack<>();

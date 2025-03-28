7bf926c2d3728fabb67dcedb64cde8fcd9eac4c5
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-03-28 06:20:04 -0700

    Time: 3 ms (21.74%), Space: 41.9 MB (20.64%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,45 +1,51 @@
 class Solution {
     public String decodeString(String s) {
-       Stack<Character> stack=new Stack();
-       Stack<Integer> time=new Stack();
-       StringBuilder final_result=new StringBuilder();
-       int num=0;
-       for(int i=0;i<s.length();i++){
-        char ch=s.charAt(i);
-        if(Character.isDigit(ch)){
-            num=num*10+(ch-'0');
-        }
-        else if(ch=='['){
-            time.push(num);
-            num=0;
-            stack.push(ch);
-        }
-        else if(ch==']'){
-            StringBuilder str= new StringBuilder();
-            StringBuilder result=new StringBuilder();
+        
+        Stack<Character> stack=new Stack();
+        Stack<Integer> time_stack=new Stack();
+        StringBuilder result=new StringBuilder();
+
+        int num=0;
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+
+            if(Character.isDigit(ch)){
+                num=num*10 +(ch-'0');
+            }
+            else if(ch=='['){
+                time_stack.push(num);
+                num=0;
+                stack.push(ch);
+
+            }
+            else if(ch==']'){
+                StringBuilder str= new StringBuilder();
+                StringBuilder ans=new StringBuilder();
                 while(!stack.isEmpty()&& stack.peek()!='['){
                     str.append(stack.pop());
                 }
-                stack.pop();
                 str.reverse();
-                for(int j=0;j<time.peek();j++){
-                    result.append(str);
+                 stack.pop();// to remove [
+                int times=time_stack.pop();
+
+                for(int j=0;j<times;j++){
+                    ans.append(str);
                 }
 
-                int times=time.pop();
+                for(char c : ans.toString().toCharArray()){
 
-            for(char c : result.toString().toCharArray())
-                stack.push(c);
+                    stack.push(c);
+                }
+
             }
-
-        else{
+            else{
                 stack.push(ch);
             }
-
         }
         while(!stack.isEmpty()){
-            final_result.append(stack.pop());
+            result.append(stack.pop());
         }
-        return final_result.reverse().toString();
+        return result.reverse().toString();
     }
 }

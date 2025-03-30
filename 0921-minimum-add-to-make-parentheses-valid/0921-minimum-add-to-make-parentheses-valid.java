9c05e6596b6bff266675b02ccd101d49b6e54a17
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-03-29 18:44:03 -0700

    Time: 2 ms (25.72%), Space: 41.8 MB (15.92%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack=new Stack();
+        for(int i=0;i<s.length();i++){
+            char ch =s.charAt(i);
+
+            if(!stack.isEmpty() && ch==')' && stack.peek()=='('){
+                stack.pop();
+            }
+            else{
+                stack.push(ch);
+            }
+
+
+
+
+        }
+        return stack.size();
+    }
+}

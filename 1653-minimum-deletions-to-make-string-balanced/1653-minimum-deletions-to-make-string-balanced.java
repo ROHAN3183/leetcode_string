35177fd6f832e0f327cfc3fd02f0e2da22e99ad6
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-03-30 15:49:57 +0530

    Update 1653-minimum-deletions-to-make-string-balanced.java

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,3 +1,51 @@
+logic of the code: -'ba' is problem in the string .
+1) just take two array so that we can compute the prefix_sum in first array take the no.of count of B's from leftside.
+2) in second array compute the count of the A's from the last index.
+3) so take the sum of the both the array of the index and the minimum will be the result that has to be deleted.
+    
+class Solution {
+    public int minimumDeletions(String s) {
+
+        int n=s.length();
+
+        int [] left_count_B=new int[n];
+        int [] right_count_A= new int[n];
+
+        int count_B=0;
+        int count_A=0;
+
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            left_count_B[i]=count_B;
+
+            if(ch=='b'){
+                count_B++;
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+            char ch=s.charAt(i);
+            right_count_A[i]=count_A;
+
+            if(ch=='a'){
+                count_A++;
+            }    
+        }
+
+        int deleted=Integer.MAX_VALUE;
+
+        for(int i=0;i<n;i++){
+            deleted=Math.min(deleted,left_count_B[i]+right_count_A[i]);
+        }
+
+    return deleted;
+
+    }
+}
+
+logic of the code: -'ba' is problem in the code
+1) just increase the count of the deleted element whenever on the top of the stack is 'b' and comming element is 'a' then pop
+    out the top of the stack also.
+    
 class Solution {
     public int minimumDeletions(String s) {
         Stack<Character> stack=new Stack();
@@ -18,3 +66,26 @@ class Solution {
         return delete;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

9177137d1664f7d57af544d70ca2539090268ad8
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-03-31 10:19:45 -0700

    Time: 5 ms (58.38%), Space: 43.3 MB (67.11%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minLength(String s) {
+        int n=s.length();
+        Stack<Character> stack=new Stack();
+        for(int i=0;i<n;i++){
+            char ch =s.charAt(i);
+            if((!stack.isEmpty()&&stack.peek()=='A' && ch=='B')||(!stack.isEmpty()&&stack.peek()=='C'&&ch=='D')){
+                stack.pop();
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+        if(stack.size()>0){
+            return stack.size();
+        }
+        return 0;
+    }
+}

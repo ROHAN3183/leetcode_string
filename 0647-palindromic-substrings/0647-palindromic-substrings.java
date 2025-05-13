9eff29791b6a7f5e4d56ba5b56fd37751bccdecd
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-05-12 23:04:34 -0700

    Time: 1083 ms (5.02%), Space: 45.4 MB (25.99%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n=s.length();
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            StringBuilder str=new StringBuilder();
+            for(int j=i;j<n;j++){
+
+                str.append(s.charAt(j));
+                char [] temp=str.toString().toCharArray();
+                if(reverse(temp)){
+                    count++;
+                }
+                
+            }
+        }
+        return count;
+    }
+    boolean reverse(char[] temp ){
+        int i=0;
+        int j=temp.length-1;
+        while(i<=j){
+            if(temp[i]!=temp[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}

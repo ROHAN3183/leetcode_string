bfb48591c4d73641ce9e68943912946296d1a2a6
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-05-13 13:51:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,33 +1,28 @@
+
 import java.util.AbstractMap;
+
 class Solution {
     public int minCost(String colors, int[] neededTime) {
-        int n1 =colors.length();
-        int n2=neededTime.length;
-        int count=0;
-        Stack<Map.Entry<Character,Integer>> stack=new Stack<>();
+        int n1 = colors.length();
+        int count = 0;
+        Stack<Map.Entry<Character, Integer>> stack = new Stack<>();
 
-        for(int i=0;i<n1;i++){
-            if(stack.isEmpty()){
-                stack.push(new AbstractMap.SimpleEntry<>(colors.charAt(i),neededTime[i]));
-            }
-            else{
-                Map.Entry<Character,Integer> temp=stack.peek();
-                if(temp.getKey()==colors.charAt(i) &&temp.getValue()<neededTime[i]){
-                    count=count+temp.getValue();
+        for (int i = 0; i < n1; i++) {
+            if (stack.isEmpty()) {
+                stack.push(new AbstractMap.SimpleEntry<>(colors.charAt(i), neededTime[i]));
+            } else {
+                Map.Entry<Character, Integer> temp = stack.peek();
+                if (temp.getKey() == colors.charAt(i) && temp.getValue() < neededTime[i]) {
+                    count += temp.getValue();
                     stack.pop();
-                    stack.push(new AbstractMap.SimpleEntry<>(colors.charAt(i),neededTime[i]));
+                    stack.push(new AbstractMap.SimpleEntry<>(colors.charAt(i), neededTime[i]));
+                } else if (temp.getKey() == colors.charAt(i) && temp.getValue() > neededTime[i]) {
+                    count += neededTime[i];
+                } else if (temp.getKey() != colors.charAt(i)) {
+                    stack.push(new AbstractMap.SimpleEntry<>(colors.charAt(i), neededTime[i]));
                 }
-                else if(temp.getKey()==colors.charAt(i) &&temp.getValue()>neededTime[i]){
-                    count=count+neededTime[i];
-                    continue;
-                }
-                else if(temp.getKey()!=colors.charAt(i)){
-                    stack.push(new AbstractMap.SimpleEntry<>(colors.charAt(i),neededTime[i]));
-                }
-
             }
         }
         return count;
-
     }
 }

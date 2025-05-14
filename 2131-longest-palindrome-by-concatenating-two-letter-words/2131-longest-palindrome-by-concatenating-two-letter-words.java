27fbd44a049cafff4228aa49fdabd723c73c22d8
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-05-14 17:49:26 +0530

    Update 2131-longest-palindrome-by-concatenating-two-letter-words.java

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,3 +1,11 @@
+Logic of the code:
+1) Maintain a hashMap to maintain the frequency.
+2) chech that rev of the string in map do list or not .
+3) After that check that orginal and reverse are not same  then calculate the size of the string.
+4) if same then add to the size in such a way that it will "(size+=leftfreq/2)*2" ex -gg gg gg gg gg there 5 pair are there but 
+    only 4 pair will make valid pair rest just again put into the hash for futher future use .
+5) check for the pair which is having 1 count like "pp" take 1 and then break; 
+
 class Solution {
     public int longestPalindrome(String[] words) {
         int n=words.length;

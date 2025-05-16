3d0bd96bb6958944ccbb6065743dc96faa71a0d5
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-05-16 21:52:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,30 +1,27 @@
-#BRUTE 
-    class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) { // Check every pair
-                if (nums[i] + nums[j] == target) { // If pair sum equals target
-                    return new int[] {i, j}; // Return the indices
-                }
-            }
-        }
-        return new int[0]; // Return empty array if no solution
-    }
-}
-
-#OPTIMIZED
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer>map=new HashMap<>();
+
         int n=nums.length;
+
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int[] result=new int[2];
+
         for(int i=0;i<n;i++){
-            int first=nums[i];
-            if(map.containsKey(target-first)){
-                return new int[]{ map.get(target-first),i};
-            }
             map.put(nums[i],i);
         }
-        return new int[]{0};
+
+        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+            int firstValue=entry.getKey();
+            int index1=entry.getValue();
+            int secondValue=Math.abs(target-firstValue);
+
+
+            if(map.containsKey(secondValue)){
+                int index2=map.get(secondValue);
+                return new int[]{index1,index2};
+            }
+        }
+        return result;
+        
     }
 }

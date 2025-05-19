3152617bb2c2500cc944a0720182d2ac8f632d05
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-05-19 21:16:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int count=0;
+
+        int [] leftCandy =new int[n];
+        int [] rightCandy=new int[n];
+        Arrays.fill(leftCandy,1);
+        Arrays.fill(rightCandy,1);
+
+        for(int i=1;i<n-1;i++){ //left-right side
+            if(ratings[i]>ratings[i-1]){
+                rightCandy[i]=rightCandy[i-1]+1;
+            }
+        }
+
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                leftCandy[i]=leftCandy[i+1]+1;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            count=count+Math.max(rightCandy[i],leftCandy[i]);
+        }
+        return count;
+        
+    }
+}

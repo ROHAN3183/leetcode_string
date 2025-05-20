f8d9afe921483b45afc9def9809a1ddceda88377
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-05-20 12:35:59 +0530

    Update 1647-minimum-deletions-to-make-character-frequencies-unique.java

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,3 +1,43 @@
+*************************************************PRIORITY QUEUE****************************************************************
+class Solution {
+    public int minDeletions(String s) {
+        int n=s.length();
+        int count=0;
+        HashMap<Character,Integer>map=new HashMap<>();
+        PriorityQueue<Integer>maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0;i<n;i++){//frequency map
+            char ch=s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for(Map.Entry<Character,Integer>entry:map.entrySet()){//build heap
+            int value=entry.getValue();
+            maxHeap.add(value);
+        }
+
+        while(maxHeap.size()>=2){//logic of code
+            int firstPeek=maxHeap.poll();
+            int secondPeek=maxHeap.poll();
+
+            if(firstPeek !=secondPeek){
+                maxHeap.add(secondPeek);
+                
+            }
+            else if(firstPeek==secondPeek){
+                firstPeek=firstPeek-1;
+                if(firstPeek >0)maxHeap.add(firstPeek);
+                if(secondPeek>0)maxHeap.add(secondPeek);
+                count++;
+            }
+        }
+    return count;
+
+    }
+}
+
+*************************************************GREEDY APPROACH**************************************************************
+    
 class Solution {
     public int minDeletions(String s) {
         int n=s.length();

81ba09fdadacdfaded75d22867819e94723d1789
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-05-20 15:02:28 +0530

    Update 2216-minimum-deletions-to-make-array-beautiful.java

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.java
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.java
@@ -1,3 +1,25 @@
+*****************************************************STACK************************************************************************
+class Solution {
+    public int minDeletion(int[] nums) {
+        int n=nums.length;
+        Stack<Integer>stack=new Stack<>();
+        for(int i=0;i<n;i++){
+            if(stack.size()%2==0){//even length
+                stack.push(nums[i]);
+            }
+            else if(stack.size()%2!=0){// odd length
+                if(stack.peek()!=nums[i]){
+                    stack.push(nums[i]);
+                }
+            }
+        }
+        if(stack.size()%2!=0){
+            stack.pop();
+        }
+        return n-stack.size();
+    }
+}
+********************************************OPTIMIZED CODE************************************************************************
 class Solution {
     public int minDeletion(int[] nums) {
         int deletions = 0;

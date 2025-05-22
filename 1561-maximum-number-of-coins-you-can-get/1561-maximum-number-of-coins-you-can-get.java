c9942088ddea93b023c34f76c60ea2dc8246c575
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-05-22 17:53:41 +0530

    Time: 106 ms (6.17%), Space: 56.6 MB (39.56%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -4,15 +4,13 @@ class Solution {
         int n = piles.length;
         Integer[] boxedPiles=Arrays.stream(piles).boxed().toArray(Integer[]::new);
         Arrays.sort(boxedPiles, Collections.reverseOrder());
-        int[] temp = new int[3];
+       // int[] temp = new int[3];
         int i = 0;
         int j = n - 1;
         int count = 0;
         while (i + 1 <= j - 1) {
-            temp[0] = boxedPiles[i];
-            temp[1] = boxedPiles[i + 1];
-            temp[2] = boxedPiles[j];
-            count = count + temp[1];
+ 
+            count = count + boxedPiles[i+1];
             i=i+2;
             j--;
         }

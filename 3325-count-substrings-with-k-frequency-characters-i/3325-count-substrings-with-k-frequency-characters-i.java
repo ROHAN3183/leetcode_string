29a8faeb885c38bcd605aee8c5de8d56188710d4
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-05-24 08:36:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3325-count-substrings-with-k-frequency-characters-i/3325-count-substrings-with-k-frequency-characters-i.java
+++ 3325-count-substrings-with-k-frequency-characters-i/3325-count-substrings-with-k-frequency-characters-i.java
@@ -1,30 +1,42 @@
 class Solution {
     public int numberOfSubstrings(String s, int k) {
-        int n = s.length();
-        int i = 0;
-        int j = 0;
-        int result = n * (n + 1) / 2;
-        if (k == 1) {
-            return n * (n + 1) / 2;
-        }
-        HashMap<Character, Integer> map = new HashMap<>();
+        int n=s.length();
+        HashMap<Character,Integer>map=new HashMap<>();
+        int i=0;
+        int j=0;
+        boolean flag=true;
+        int result=0;
 
-        while (j < n) {
-            char ch = s.charAt(j);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        while(j<n){
+            char ch =s.charAt(j);
 
-            while (map.get(ch) >= k) {
-                char left = s.charAt(i);
-                map.put(left, map.get(left) - 1);
-                if (map.get(left) == 0) {
-                    map.remove(left);
-                }
-                i++;
+           if(map.containsKey(ch) && map.get(ch)!=-1){
+             map.put(ch,map.getOrDefault(ch,0)+1);
             }
-            result = result - (j - i + 1);
+            else{
+                map.put(ch,map.getOrDefault(ch,0)+1);
+            }
+
+            if(map.containsKey(ch) && map.get(ch)>=k){
+                char firstTime=s.charAt(i);
+                while(s.charAt(i)!=(ch)){
+                    i++;
+                }
+                if(flag){
+                    result=result+ n-(j-i);
+                    flag=!flag;//first count both forword and the back
+                }
+                else{
+                    result+=n-j;
+                }
+                i=0; // to again find the starting point of the character 
+                map.put(ch,-1);//to mark that it has been used.
+
+            }
             j++;
+
         }
         return result;
-
+        
     }
 }

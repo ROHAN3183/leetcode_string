78b052e33eb267183c6a029fd6a2f6e0b590428b
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-05-25 12:48:42 +0530

    Update 0856-score-of-parentheses.java

--- 0856-score-of-parentheses/0856-score-of-parentheses.java
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -1,33 +1,31 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        int n=s.length();
-        int score =0;
-        Stack<Integer> stack=new Stack<>();
+        int n = s.length();
+        int score = 0;
+        Stack<Integer> stack = new Stack<>();
 
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            if(ch=='('){
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
                 stack.push(0);
-            }
-            else if(!stack.isEmpty() && stack.peek()==0 && ch==')'){
+            } else if (!stack.isEmpty() && stack.peek() == 0 && ch == ')') {
                 stack.pop();
                 stack.push(1);
-            }
-            else if(!stack.isEmpty() && stack.peek()>0 && ch==')'){
-                int sum=0;
-                while(!stack.isEmpty() && stack.peek()>0){
-                    sum=sum+stack.pop();
+            } else if (!stack.isEmpty() && stack.peek() > 0 && ch == ')') {
+                int sum = 0;
+                while (!stack.isEmpty() && stack.peek() > 0) {
+                    sum = sum + stack.pop();
                 }
                 if (!stack.isEmpty() && stack.peek() == 0) {
-                    stack.pop(); // pop the matching '(' marker
+                    stack.pop();
                 }
-                stack.push(2*sum);
+                stack.push(2 * sum);
             }
         }
-        while(!stack.isEmpty()){
-            score=score+stack.pop();
+        while (!stack.isEmpty()) {
+            score = score + stack.pop();
         }
         return score;
-        
+
     }
 }

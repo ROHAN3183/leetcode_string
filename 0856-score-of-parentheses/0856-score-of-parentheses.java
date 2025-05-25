2de55d91e3b5438ee9d9ef8c7621c343e69541c0
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-05-25 11:22:39 +0530

    Time: 1 ms (44.97%), Space: 42.1 MB (6.9%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.java
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -1,19 +1,36 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        int n=s.length();
-        Stack<Character>stack=new Stack<>();
-        int score =0;
+        int n = s.length();
+        Stack<Character> stack = new Stack<>();
+        int score = 0;
 
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            if(!stack.isEmpty() && stack.peek()=='(' && ch==')'){
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            if (!stack.isEmpty() && stack.peek() == '(' && ch == ')') {
                 stack.pop();
-                score++;
-            }
-            else{
-                stack.push(ch);
+                stack.push('1'); // Found ()
+            } else if (ch == '(') {
+                stack.push('(');
+            } else if (!stack.isEmpty() && ch == ')') {
+                int sum = 0;
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    char temp = stack.pop();
+                    sum += (int)(temp - '0');  // Convert char to int
+                }
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop(); // remove '('
+                }
+                sum *= 2;
+                stack.push((char)(sum + '0')); // Convert int to char
             }
         }
+
+        // Sum all characters in stack
+        while (!stack.isEmpty()) {
+            score += (int)(stack.pop() - '0');
+        }
+
         return score;
     }
 }

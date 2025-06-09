2ac7728b8af1cd7341de07ab93609e9170bdf402
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-06-09 19:54:37 +0530

    Time: 0 ms (100%), Space: 42.5 MB (15.4%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -1,44 +1,18 @@
 class Solution {
     public int maxAncestorDiff(TreeNode root) {
-        List<Integer>result=new ArrayList<>();
-        Queue<TreeNode>queue=new LinkedList<>();
-        if(root!=null){
-            queue.add(root);
-        }
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                TreeNode node=queue.poll();
-                int maxVal=node.val;
-                preOrder(node,maxVal,result);
-                if(node.left!=null){
-                    queue.add(node.left);
-                }
-                if(node.right!=null){
-                    queue.add(node.right);
-                }
-
-            }
-        }
-        int max=Collections.max(result);
-        return max;
-        
-        
-    }
-
-    void preOrder(TreeNode node, int maxVal, List<Integer> result) {
-        if (node == null) {
-            return;
-        }
-        if (node.left != null) {
-            preOrder(node.left, maxVal, result);
-        }
-        if (node.right != null) {
-            preOrder(node.right, maxVal, result);
-        }
-        int diff = Math.abs(maxVal- (int)node.val);
-        result.add(diff);
+        int result = maxAncestor(root, root.val, root.val);
+        return result;
 
     }
 
+    int maxAncestor(TreeNode root, int maxVal, int minVal) {
+        if (root == null) {
+            return Math.abs(maxVal - minVal);
+        }
+        maxVal = Math.max(maxVal, root.val);
+        minVal = Math.min(minVal, root.val);
+        int left = maxAncestor(root.left, maxVal, minVal);
+        int right = maxAncestor(root.right, maxVal, minVal);
+        return Math.max(left, right);
+    }
 }

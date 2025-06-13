da2a71b56016e41638a76f8058e15ddf4f680541
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-06-13 11:39:04 +0530

    Time: 0 ms (100%), Space: 42.6 MB (8.14%) - LeetHub

---
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.java
@@ -0,0 +1,24 @@
+class Solution {
+    int moves;
+
+    public int distributeCoins(TreeNode root) {
+        moves = 0;
+        dfs(root);
+        return moves;
+
+    }
+
+    int dfs(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+        int leftmoves = dfs(root.left);
+
+        int rightmoves = dfs(root.right);
+
+        moves += Math.abs(leftmoves) + Math.abs(rightmoves);
+
+        return leftmoves + rightmoves + root.val - 1;
+    }
+}

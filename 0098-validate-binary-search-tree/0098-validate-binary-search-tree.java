be4d746b88ca8feb28009950a2ed1a18d7d411b0
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-06-15 15:38:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+        
+    }
+    boolean dfs(TreeNode root,int min,int max){
+        if(root==null){
+            return true;
+        }
+        if(root.val>=max || root.val<=min){
+            return false;
+        }
+        return dfs(root.left,min,root.val) && dfs(root.right,root.val,max);
+    }
+}

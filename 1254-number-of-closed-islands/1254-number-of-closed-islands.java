d9c4335c7cd7bceb853a2ee8ff08c6ebeb078de5
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-06-18 10:27:54 +0530

    Time: 2 ms (100%), Space: 44.4 MB (70.43%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -3,12 +3,12 @@ class Solution {
 
         int m = grid.length;
         int n = grid[0].length;
-        int count=0;
+        int count = 0;
 
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
                 if (grid[r][c] == 0) {
-                    if(dfs(grid, r, c)==1){
+                    if (dfs(grid, r, c)) {
                         count++;
                     }
                 }
@@ -17,26 +17,26 @@ class Solution {
         return count;
     }
 
-    int dfs(int[][] grid, int r, int c) {
+    boolean dfs(int[][] grid, int r, int c) {
         if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length) {
-            return 0;
+            return false;
         }
-        if(grid[r][c]==1){
-            return 1;
+
+        if (grid[r][c] == 1) {
+            return true;
         }
+
         if (grid[r][c] == 2) {
-            return 1;
+            return true;
         }
+
         grid[r][c] = 2;
 
-        int bottom = dfs(grid, r + 1, c);
-        int top = dfs(grid, r - 1, c);
-        int right = dfs(grid, r, c + 1);
-        int left = dfs(grid, r, c - 1);
+        boolean bottom = dfs(grid, r + 1, c);
+        boolean top = dfs(grid, r - 1, c);
+        boolean right = dfs(grid, r, c + 1);
+        boolean left = dfs(grid, r, c - 1);
 
-        if(left==1 && right==1 &&top==1 &&bottom==1){
-            return 1;
-        }
-        return 0;
+        return bottom && top && right && left;
     }
 }

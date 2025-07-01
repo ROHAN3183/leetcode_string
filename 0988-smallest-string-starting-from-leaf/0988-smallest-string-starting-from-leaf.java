58356a784a8835943c1b476c057ed80abab1a130
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-01 21:15:38 +0530

    Time: 2 ms (88.15%), Space: 44.7 MB (72.7%) - LeetHub

---
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    String result="";
+    public String smallestFromLeaf(TreeNode root) {
+        StringBuilder ans=new StringBuilder();
+        dfs(root,ans);
+        return result;
+    }
+    void dfs(TreeNode root,StringBuilder ans){
+        if(root==null){
+            return ;
+        }
+        ans.insert(0,(char)(root.val+'a'));
+        if(root.left==null && root.right==null){
+            if(result==""||ans.toString().compareTo(result)<0){
+                result=ans.toString();
+            }
+        }
+        dfs(root.left,ans);
+        dfs(root.right,ans);
+        ans.deleteCharAt(0);
+    }
+}

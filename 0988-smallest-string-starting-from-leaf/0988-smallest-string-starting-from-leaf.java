c55c298aed6c8db1fb08b43d024062279266d7f8
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-01 21:19:55 +0530

    Time: 1 ms (99.87%), Space: 44.5 MB (84.42%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -1,23 +1,24 @@
-
 class Solution {
-    String result="";
+    String result = "";
+
     public String smallestFromLeaf(TreeNode root) {
-        StringBuilder ans=new StringBuilder();
-        dfs(root,ans);
+        StringBuilder ans = new StringBuilder();
+        dfs(root, ans);
         return result;
     }
-    void dfs(TreeNode root,StringBuilder ans){
-        if(root==null){
-            return ;
+
+    void dfs(TreeNode root, StringBuilder ans) {
+        if (root == null) {
+            return;
         }
-        ans.insert(0,(char)(root.val+'a'));
-        if(root.left==null && root.right==null){
-            if(result==""||ans.toString().compareTo(result)<0){
-                result=ans.toString();
+        ans.insert(0, (char) (root.val + 'a'));
+        if (root.left == null && root.right == null) {
+            if (result == "" || ans.toString().compareTo(result) < 0) {
+                result = ans.toString();
             }
         }
-        dfs(root.left,ans);
-        dfs(root.right,ans);
+        dfs(root.left, ans);
+        dfs(root.right, ans);
         ans.deleteCharAt(0);
     }
 }

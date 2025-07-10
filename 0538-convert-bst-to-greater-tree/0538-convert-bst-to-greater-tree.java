5a01287671ceca518f95b7c2f310a994e4124d71
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-10 21:17:56 +0530

    Time: 0 ms (100%), Space: 45.3 MB (63.66%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -1,17 +1,19 @@
 class Solution {
-    int sum=0;
+    int sum = 0;
+
     public TreeNode convertBST(TreeNode root) {
         dfs(root);
         return root;
-        
+
     }
-    void dfs(TreeNode root){
-        if(root==null){
-            return ;
+
+    void dfs(TreeNode root) {
+        if (root == null) {
+            return;
         }
         dfs(root.right);
-        sum=sum+root.val;
-        root.val=sum;
+        sum = sum + root.val;
+        root.val = sum;
         dfs(root.left);
 
     }

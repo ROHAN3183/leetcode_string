d240eabf6228b3402895350313a88cf8d0e808ce
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-14 16:37:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    class Pair {
+        int distance;
+        double time;
+
+        Pair(int distance, double time) {
+            this.distance = distance;
+            this.time = time;
+        }
+
+    }
+
+    public int carFleet(int target, int[] position, int[] speed) {
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<>((a, b) -> b.distance - a.distance);
+        int fleet=0;
+        int size = position.length;
+        for (int i = 0; i < size; i++) {
+            int d = position[i];
+            double time = (double)position[i] / speed[i];
+            maxHeap.offer(new Pair(d, time));
+        }
+        double minTime=-1;
+        while(!maxHeap.isEmpty()){
+            Pair popped=maxHeap.poll();
+            if(popped.time>minTime){
+                fleet++;
+                minTime=popped.time;
+            }
+
+        }
+        return fleet;
+
+    }
+}

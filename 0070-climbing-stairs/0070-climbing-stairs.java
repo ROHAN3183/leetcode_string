83ecef70cc4c2e253af914cdaf8e4be30286e50f
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-16 06:13:35 +0530

    Update 0070-climbing-stairs.java

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,3 +1,29 @@
+/*LOGIC OF THE CODE :
+Similar to the fibonaccai series 
+*/
+*********************************************MEMORIZATION***********************************************************************
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return DP(n, dp);
+
+    }
+
+    int DP(int n, int[] dp) {
+        if (n <= 1) {
+            return 1;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = DP(n - 1, dp) + DP(n - 2, dp);
+        
+        return dp[n];
+    }
+}
+************************************************TABULATION METHOD****************************************************************
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
@@ -18,5 +44,30 @@ class Solution {
 
         return dp[n];
 
+    }
+}
+****************************************************SPACE OPTIMIZATION**********************************************************
+class Solution {
+    public int climbStairs(int n) {
+        return DP(n);
+    }
+
+    int DP(int n) {
+
+        if (n <= 1) {
+            return 1;
+        }
+
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr = 0;
+
+        for (int i = 2; i <= n; i++) {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return curr;
     }
 }

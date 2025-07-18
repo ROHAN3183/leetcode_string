d52c0615072069b75494591acbf7acc8b4f47354
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-18 14:35:58 +0530

    Time: 1 ms (70.65%), Space: 50 MB (28.06%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,20 +1,58 @@
 class Solution {
+    int min;
+    int max;
+
     public int kthSmallest(int[][] matrix, int k) {
 
-        PriorityQueue<Integer>maxHeap=new PriorityQueue<>(Collections.reverseOrder());
-        int n=matrix.length;
+        findMinAndMax(matrix);
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                maxHeap.add(matrix[i][j]);
+        int low = min;
+        int high = max;
+        int result = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+            int smallestCount = 0;
+
+            for (int i = 0; i < matrix.length; i++) {
+                smallestCount = smallestCount + numberOfSmallest(matrix[i], mid);
+            }
+
+            if (smallestCount >= k) {
+                result = mid;
+                high = mid - 1;
+
+            } else {
+                low = mid + 1;
             }
         }
+        return result;
 
-        while(maxHeap.size()>k){
-            maxHeap.poll();
-        }
-
-        return maxHeap.peek();
-        
     }
+
+    void findMinAndMax(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            min = Math.min(matrix[i][0], min);
+            max = Math.max(matrix[i][matrix.length - 1], max);
+        }
+    }
+
+    int numberOfSmallest(int[] matrix, int target) {
+
+        int low = 0;
+        int high = matrix.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (matrix[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
 }

d256239af42f10bbe90037c0a26560193c40bfc6
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-19 17:17:51 +0530

    Time: 252 ms (8.86%), Space: 68.2 MB (51.43%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,22 +1,37 @@
 class Solution {
+    class Pair {
+        long sum;
+        int idx;
+
+        Pair(long sum, int idx) {
+            this.sum = sum;
+            this.idx = idx;
+        }
+    }
+
     public int rangeSum(int[] nums, int n, int left, int right) {
-        int size = nums.length;
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>((a, b) -> Long.compare(a.sum, b.sum));
+
         List<Long> list = new ArrayList<>();
+        long result = 0;
         int mod = 1_000_000_007;
-        for (int i = 0; i < size; i++) {
-            long sum = 0;
-            for (int j = i; j < size; j++) {
-                sum = (sum + nums[j]) % mod;
-                list.add(sum);
+
+        for (int i = 0; i < nums.length; i++) {
+            minHeap.add(new Pair((long) nums[i], i));
+        }
+
+        while (!minHeap.isEmpty()) {
+            Pair pair = minHeap.poll();
+            list.add(pair.sum);
+            if (pair.idx + 1 < n) {
+                minHeap.add(new Pair(pair.sum + nums[pair.idx + 1], pair.idx + 1));
             }
         }
-        Collections.sort(list);
-        long sum = 0;
-
-        for (int i = left - 1; i < right; i++) {
-            sum = (sum + list.get(i)) % mod;
+        for (int i = 0; i < list.size(); i++) {
+            if (i >= left - 1 && i < right) {
+                result = (result + list.get(i)) % mod;
+            }
         }
-        return (int) sum;
-
+        return (int) result;
     }
 }

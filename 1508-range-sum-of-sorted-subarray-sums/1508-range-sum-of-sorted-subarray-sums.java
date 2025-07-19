cb9aca320d8e7d9aa6ca07d4f77a696828c2fd11
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-19 17:26:46 +0530

    Update 1508-range-sum-of-sorted-subarray-sums.java

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,3 +1,34 @@
+
+***********************************************BRUTE FORCE**********************************************************************
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        int size = nums.length;
+        List<Long> list = new ArrayList<>();
+        int mod = 1_000_000_007;
+        for (int i = 0; i < size; i++) {
+            long sum = 0;
+            for (int j = i; j < size; j++) {
+                sum = (sum + nums[j]) % mod;
+                list.add(sum);
+            }
+        }
+        Collections.sort(list);
+        long sum = 0;
+
+        for (int i = left - 1; i < right; i++) {
+            sum = (sum + list.get(i)) % mod;
+        }
+        return (int) sum;
+
+    }
+}
+................................................................................................................................
+/*logic of the code : 
+interesting property of the priorityQueue is that it can give the sum of all the possible subarray in the sorted order.
+it can be achieve by first add all the element with the index and after that pop the peek and add to the sum  and after that 
+push the next index of the poped with the (sum ,index ).
+*/
+******************************************************OPTIMIZED CODE************************************************************
 class Solution {
     class Pair {
         long sum;

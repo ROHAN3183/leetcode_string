c7d539d460987f68c16f065cb4c5c38e9b1ae407
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-20 08:09:04 +0530

    Update 0746-min-cost-climbing-stairs.java

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,10 +1,39 @@
+/*LOGIC OF THE CODE -> there are only 2 possibility their we can start from the 0 index or 1 index so we have to write  the 
+two funcion.
+add the current cost + dfs(cost,idx+1 ) dfs(cost ,idx+2) min of the each
+*/
+**********************************************MEMORIZATION**********************************************************************
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[1001];
+        Arrays.fill(dp, -1);
+        return Math.min(dfs(cost, 0, dp), dfs(cost, 1, dp));
+    }
+
+    int dfs(int[] cost, int idx, int[] dp) {
+        if (idx >= cost.length) {
+            return 0;
+
+        }
+        if (dp[idx] != -1) {
+            return dp[idx];
+        }
+        int oneStep = cost[idx] + dfs(cost, idx + 1, dp);
+
+        int twoSteps = cost[idx] + dfs(cost, idx + 2, dp);
+
+        return dp[idx] = Math.min(oneStep, twoSteps);
+
+    }
+}
+*********************************************SPACE OPTIMIZED CODE*****************************************************************
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int prev1 = 0;
         int prev2 = 0;
         for (int i = cost.length - 1; i >= 0; i--) {
 
-            int curr = cost[i] + Math.min(prev1, prev1);
+            int curr = cost[i] + Math.min(prev1, prev2);
             prev2 = prev1;
             prev1 = curr;
 

edc69ec21f9b2c2018b829ce0e5f268470be7848
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-20 08:02:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,23 +1,14 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[1001];
-        Arrays.fill(dp, -1);
-        return Math.min(dfs(cost, 0, dp), dfs(cost, 1, dp));
-    }
+        int prev1 = 0;
+        int prev2 = 0;
+        for (int i = cost.length - 1; i >= 0; i--) {
 
-    int dfs(int[] cost, int idx, int[] dp) {
-        if (idx >= cost.length) {
-            return 0;
+            int curr = cost[i] + Math.min(prev1, prev1);
+            prev2 = prev1;
+            prev1 = curr;
 
         }
-        if (dp[idx] != -1) {
-            return dp[idx];
-        }
-        int oneStep = cost[idx] + dfs(cost, idx + 1, dp);
-
-        int twoSteps = cost[idx] + dfs(cost, idx + 2, dp);
-
-        return dp[idx] = Math.min(oneStep, twoSteps);
-
+        return Math.min(prev1, prev2);
     }
 }

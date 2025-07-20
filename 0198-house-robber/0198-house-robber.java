3be62b90d67e7ed52f2a87a1e0daa1e1f6701001
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-20 09:49:57 +0530

    Update 0198-house-robber.java

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,4 +1,9 @@
-class Solution {
+/*LOGIC OF THE CODE
+if taking nums[i] then call the dfs(i+2) because cannot take the adjacent house 
+if not taking then call the dfs(i+1) because current house not taken.
+*/
+********************************************MEMORIZATION*************************************************************************
+class Solution{
     public int rob(int[] nums) {
         int[] dp = new int[101];
         Arrays.fill(dp, -1);
@@ -16,5 +21,33 @@ class Solution {
         int second = 0 + dfs(nums, idx + 1, dp);
 
         return dp[idx] = Math.max(first, second);
+    }
+}
+****************************************SPACE OPTIMIZATION************************************************************************
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return nums[0];
+        }
+
+        int prev1 = 0;
+        int prev2 = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            int notSkip = nums[i] + prev2;
+            int toSkip = prev1;
+
+            int curr = Math.max(notSkip, toSkip);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
     }
 }

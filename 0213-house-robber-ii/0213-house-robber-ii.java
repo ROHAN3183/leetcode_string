8b20248a3abe717fa89b700428c5551025211c5b
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-20 11:03:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,40 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp1 = new int[101];
-        int [] dp2=new int [101];
-        Arrays.fill(dp1,-1);
-        Arrays.fill(dp2,-1);
-        if (nums.length == 0)
-            return 0;
-        if (nums.length <= 1)
-            return nums[0];
+        int n = nums.length;
 
-        return Math.max(dfs1(nums, 0, dp1), dfs2(nums, 1, dp2));
+        return Math.max(robLinear(nums, n - 2, 0), robLinear(nums, n - 1, 1));
     }
 
-    int dfs1(int[] nums, int idx, int[] dp1) {
-        if (idx >= nums.length - 1) {
-            return 0;
-        }
-        if (dp1[idx] != -1) {
-            return dp1[idx];
-        }
-        int take = nums[idx] + dfs1(nums, idx + 2, dp1);
-        int skip = 0 + dfs1(nums, idx + 1, dp1);
+    int robLinear(int[] nums, int start, int end) {
 
-        return dp1[idx] = Math.max(take, skip);
-    }
+        int prev1 = 0;
+        int prev2 = 0;
 
-    int dfs2(int[] nums, int idx, int[] dp2) {
-        if (idx >= nums.length) {
-            return 0;
-        }
-        if (dp2[idx] != -1) {
-            return dp2[idx];
-        }
-        int take = nums[idx] + dfs2(nums, idx + 2, dp2);
-        int skip = 0 + dfs2(nums, idx + 1, dp2);
+        for (int i = start; i >= end; i--) {
 
-        return dp2[idx] = Math.max(take, skip);
+            int take = nums[i] + prev2;
+            int skip = 0 + prev1;
+
+            int curr = Math.max(take, skip);
+            prev2 = prev1;
+            prev1 = curr;
+
+        }
+
+        return prev1;
     }
 }

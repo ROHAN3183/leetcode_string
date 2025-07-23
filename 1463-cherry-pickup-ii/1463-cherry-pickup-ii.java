1017070c6d3886c149d87a0c3c0945c462bfebe0
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-23 06:13:10 +0530

    Time: 21 ms (93.96%), Space: 46.7 MB (10.51%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -19,15 +19,15 @@ class Solution {
         if (col1 != col2) {
             result += grid[row][col2];
         }
+        int max = 0;
         if (row + 1 < grid.length) {
-            int max = 0;
             for (int i = -1; i <= 1; i++) {
                 for (int j = -1; j <= 1; j++) {
                     max = Math.max(max, dfs(grid, row + 1, col1 + i, col2 + j, dp));
                 }
             }
-            result += max;
         }
+        result += max;
         dp[row][col1][col2] = result;
         return result;
 

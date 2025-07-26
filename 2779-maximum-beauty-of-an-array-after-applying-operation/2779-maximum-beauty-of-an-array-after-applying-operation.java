aec832fdf54b5b0759d6d7f7fdeac85bce9effda
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-26 17:39:07 +0530

    Time: 251 ms (8.79%), Space: 74.9 MB (5.17%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,31 +1,33 @@
 class Solution {
+    class Pair {
+        int start;
+        int end;
+
+        Pair(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
     public int maximumBeauty(int[] nums, int k) {
+        Deque<Pair> dq = new LinkedList<>();
         List<List<Integer>> list = new ArrayList<>();
+        int size = 0;
         for (int i = 0; i < nums.length; i++) {
             list.add(Arrays.asList(nums[i] - k, nums[i] + k));
         }
         Collections.sort(list, (a, b) -> Integer.compare(a.get(0), b.get(0)));
-        int max = 1;
-        int count = 1;
 
-        for (int i = 0; i < list.size(); i++) {
-            int prevStart=list.get(i).get(0);
-            int prevEnd=list.get(i).get(1);
-            count =1;
-            for(int j=i+1 ;j<list.size();j++){
-                int currStart =list.get(j).get(0);
-                int currEnd =list.get(j).get(1);
-                if(prevEnd >=currStart){
-                    count++;
-                    prevStart=Math.max(currStart ,prevStart);
-                    prevEnd=Math.min(currEnd,prevEnd);
-                }
-                else{
-                    break;
-                }
+        for (int i = 0; i < nums.length; i++) {
+
+            while (!dq.isEmpty() && dq.peekFirst().end < list.get(i).get(0)) {
+                dq.pollFirst();
             }
-            max=Math.max(max,count);
+            dq.offerLast(new Pair(list.get(i).get(0), list.get(i).get(1)));
+            size = Math.max(size, dq.size());
+
         }
-        return max;
+        return size;
+
     }
 }

b47457e2b87b89ae29a40f0fb86675326a039130
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-28 17:57:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,6 +1,4 @@
-
 class Solution {
-
     class Pair {
         int difficult;
         int profit;
@@ -13,29 +11,32 @@ class Solution {
 
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
         List<Pair> list = new ArrayList<>();
-        int ans = 0;
-
         for (int i = 0; i < difficulty.length; i++) {
             list.add(new Pair(difficulty[i], profit[i]));
         }
-
         Collections.sort(list, (a, b) -> a.difficult - b.difficult);
         Arrays.sort(worker);
-
-        int j = 0;
-        int best = 0;
-
+        int result = 0;
         for (int i = 0; i < worker.length; i++) {
             int value = worker[i];
-
-            while (j < list.size() && list.get(j).difficult <= value) {
-                best = Math.max(best, list.get(j).profit);
-                j++;
-            }
-
-            ans += best;
+            result += binarySearch(list, value);
         }
+        return result;
+    }
 
-        return ans;
+    int binarySearch(List<Pair> list, int value) {
+        int low = 0;
+        int high = list.size() - 1;
+        int max = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (list.get(mid).difficult <= value) {
+                max = Math.max(max, list.get(mid).profit);
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return max;
     }
 }

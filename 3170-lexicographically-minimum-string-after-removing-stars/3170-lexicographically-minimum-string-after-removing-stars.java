0e45273e53fddbb39ddd06957fefd95a974c7ef3
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-08-07 18:32:57 +0530

    Time: 255 ms (10.75%), Space: 56.7 MB (5.03%) - LeetHub

--- 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.java
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.java
@@ -10,7 +10,7 @@ class Solution {
     }
 
     public String clearStars(String s) {
-        List<Integer> list = new ArrayList<>();
+        Set<Integer> deletedIdx = new HashSet<>();
         PriorityQueue<Pair> minHeap = new PriorityQueue<>((a, b) -> {
             if (a.value == b.value) {
                 return b.idx - a.idx;
@@ -18,41 +18,24 @@ class Solution {
                 return a.value - b.value;
             }
         });
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (Character.isLetter(ch)) {
-                map.put(ch, map.getOrDefault(ch, 0) + 1);
-            }
-        }
+
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (Character.isLetter(ch)) {
                 minHeap.offer(new Pair(ch, i));
             } else {
                 Pair delete = minHeap.poll();
-                int freq = map.get(delete.value);
-                if (freq == 1) {
-                    map.remove(delete.value);
-                } else {
-                    map.put(delete.value, freq - 1);
-                }
-                list.add(delete.idx);
+                deletedIdx.add(delete.idx);
             }
         }
+
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (Character.isLetter(ch) && map.containsKey(ch) && !list.contains(i)) {
-                str.append(ch);
-                int freq = map.get(ch);
-                if (freq == 1) {
-                    map.remove(ch);
-                } else {
-                    map.put(ch, freq - 1);
-                }
+            if (Character.isLetter(s.charAt(i)) && !deletedIdx.contains(i)) {
+                str.append(s.charAt(i));
             }
         }
+
         return str.toString();
     }
 }

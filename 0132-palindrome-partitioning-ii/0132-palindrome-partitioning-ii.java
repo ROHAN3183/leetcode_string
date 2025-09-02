554f510bc8ed133d1151afdc3e006ff00715dc63
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-09-02 15:56:02 +0530

    Time: 2516 ms (20.38%), Space: 42.1 MB (61.85%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    Integer[] dp;
+
+    public int minCut(String s) {
+        dp = new Integer[2001];
+        return dfs(s, 0) - 1;
+    }
+
+    int dfs(String s, int i) {
+        if (i == s.length()) {
+            return 0;
+        }
+        if (dp[i] != null) {
+            return dp[i];
+        }
+
+        int result = Integer.MAX_VALUE;
+        for (int j = i; j < s.length(); j++) {
+            if (isValid(s, i, j)) {
+                int min = 1 + dfs(s, j + 1);
+                result = Math.min(result, min);
+            }
+        }
+        return dp[i] = result;
+    }
+
+    boolean isValid(String s, int i, int j) {
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+}

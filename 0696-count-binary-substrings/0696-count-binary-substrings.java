529e26e2bc146a7254b8f24348e5b232c53922cd
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-09-07 23:20:26 +0530

    Time: 10 ms (89.13%), Space: 45.4 MB (29.61%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.java
+++ 0696-count-binary-substrings/0696-count-binary-substrings.java
@@ -1,42 +1,21 @@
 class Solution {
     public int countBinarySubstrings(String s) {
         int n = s.length();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            String str = "";
-            for (int j = i; j < n; j++) {
-                str = str + s.charAt(j);
+        int prevCount = 0;
+        int currCount = 1;
+        int result = 0;
 
-                if (isValid(str) && str.length()%2==0) {
-                    count++;
-                }
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                currCount++;
+            } else {
+                result += Math.min(prevCount, currCount);
+                prevCount = currCount;
+                currCount = 1;
             }
         }
-        return count;
-    }
 
-    boolean isValid(String str) {
-        char ch = str.charAt(0);
-        int n = str.length();
-
-        if (n % 2 != 0) {
-            return false;
-        }
-        return compute(str, '0', '1') || compute(str, '1', '0');
-    }
-
-    boolean compute(String str, char ch1, char ch2) {
-        int n = str.length();
-        for (int i = 0; i < n / 2; i++) {
-            if (str.charAt(i) == ch2) {
-                return false;
-            }
-        }
-        for (int i = n / 2; i < n; i++) {
-            if (str.charAt(i) == ch1) {
-                return false;
-            }
-        }
-        return true;
+        result += Math.min(prevCount, currCount);
+        return result;
     }
 }

0508fc76bff0315b4f8effe9101f633e334f4f36
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-15 00:14:53 +0530

    Time: 1 ms (100%), Space: 44.4 MB (80.88%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
@@ -3,14 +3,14 @@ class Solution {
         int i = 0;
         int j = 0;
         int n = nums.size();
-        while (i <= n-2*k) {
+        while (i <= n - 2 * k) {
 
             while (j < n && j - i + 1 < 2 * k) {
                 j++;
             }
 
             if (j - i + 1 == 2 * k) {
-                if (isValid(nums, i, i + k) && isValid(nums, i + k,i+2*k)) {
+                if (isValid(nums, i, i + k) && isValid(nums, i + k, i + 2 * k)) {
                     return true;
                 }
             }
@@ -21,8 +21,8 @@ class Solution {
     }
 
     boolean isValid(List<Integer> nums, int low, int high) {
-        for (int i = low; i < high-1; i++) {
-            if (i+1<nums.size() &&nums.get(i) >= nums.get(i + 1)) {
+        for (int i = low; i < high - 1; i++) {
+            if (i + 1 < nums.size() && nums.get(i) >= nums.get(i + 1)) {
                 return false;
             }
         }

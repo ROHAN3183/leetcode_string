ecd504e53fa5bc4d1bb4836cdc0f8f7aa3a44106
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-17 18:40:51 +0530

    Time: 0 ms (100%), Space: 47.9 MB (47.37%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -3,33 +3,20 @@ class Solution {
         int n = isConnected.length;
         int count = 0;
         boolean[] visited = new boolean[n];
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
-        for (int i = 0; i < visited.length; i++) {
             if (!visited[i]) {
+                dfs(isConnected, visited, i);
                 count++;
-                dfs(i, adj, visited);
             }
         }
         return count;
-
     }
 
-    void dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] visited) {
-        visited[node] = true;
-        for (int neibour : adj.get(node)) {
-            if (!visited[neibour]) {
-                dfs(neibour, adj, visited);
+    void dfs(int[][] isConnected, boolean[] visited, int i) {
+        visited[i] = true;
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                dfs(isConnected, visited, j);
             }
         }
     }

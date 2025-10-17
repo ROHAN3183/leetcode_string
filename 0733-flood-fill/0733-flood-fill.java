3be991f91434a61eedf77e9c05a91674129b39b4
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-17 23:11:01 +0530

    Time: 0 ms (100%), Space: 45.3 MB (11.2%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,51 +1,32 @@
 class Solution {
-    class Pair {
-        int i;
-        int j;
 
-        Pair(int i, int j) {
-            this.i = i;
-            this.j = j;
-        }
-    }
+    int startColor;
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n = image.length;
-        int m = image[0].length;
-        Queue<Pair> queue = new LinkedList<>();
-        boolean visited[][] = new boolean[n][m];
-        int OriginalColor=image[sr][sc];
-        if(OriginalColor==color){
-            return image;
-        }
-        queue.offer(new Pair(sr, sc));
-        image[sr][sc]=color;
-        while (!queue.isEmpty()) {
-            Pair node = queue.poll();
-            int i = node.i;
-            int j = node.j;
 
-            if (i - 1 >= 0 && image[i - 1][j] == OriginalColor && !visited[i - 1][j]) {
-                visited[i - 1][j] = true;
-                image[i - 1][j] = color;
-                queue.offer(new Pair(i - 1, j));
-            }
-            if (i + 1 < n && image[i + 1][j] == OriginalColor && !visited[i + 1][j]) {
-                visited[i + 1][j] = true;
-                image[i + 1][j] = color;
-                queue.offer(new Pair(i + 1, j));
-            }
-            if (j - 1 >= 0 && image[i][j - 1] == OriginalColor && !visited[i][j - 1]) {
-                visited[i][j - 1] = true;
-                image[i][j - 1] = color;
-                queue.offer(new Pair(i, j - 1));
-            }
-            if (j + 1 < m && image[i][j + 1] == OriginalColor && !visited[i][j + 1]) {
-                visited[i][j + 1] = true;
-                image[i][j + 1] = color;
-                queue.offer(new Pair(i, j + 1));
-            }
+        startColor = image[sr][sc];
+        if (image[sr][sc] != color) {
+            dfs(image, sr, sc, color);
         }
         return image;
+
+    }
+
+    void dfs(int[][] image, int sr, int sc, int color) {
+        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length) {
+            return;
+        }
+
+        if (image[sr][sc] != startColor) {
+            return;
+        }
+        image[sr][sc] = color;
+
+        dfs(image, sr, sc + 1, color);
+        dfs(image, sr, sc - 1, color);
+        dfs(image, sr + 1, sc, color);
+        dfs(image, sr - 1, sc, color);
+
+        return;
     }
 }

de7aaba06b468cc1360239df44d3a8148ae145ba
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-18 10:24:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,35 +1,59 @@
 class Solution {
+    class Pair {
+        int i;
+        int j;
+
+        Pair(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+    }
+
     public int maxAreaOfIsland(int[][] grid) {
-
-        int m = grid.length;
-        int n = grid[0].length;
+        int n = grid.length;
+        int m = grid[0].length;
         int max = 0;
-
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (grid[r][c] == 1) {
-                    max = Math.max(max, dfs(grid, r, c));
+        Queue<Pair> queue = new LinkedList<>();
+        boolean visited[][] = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    max = Math.max(max, bfs(grid, visited, queue, i, j));
                 }
             }
         }
         return max;
+
     }
 
-    int dfs(int[][] grid, int r, int c) {
-        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length) {
-            return 0;
+    int bfs(int[][] grid, boolean[][] visited, Queue<Pair> queue, int row, int col) {
+        int n = grid.length;
+        int m = grid[0].length;
+        queue.offer(new Pair(row, col));
+        visited[row][col] = true;
+        int count = 0;
+        while (!queue.isEmpty()) {
+            Pair node = queue.poll();
+            int i = node.i;
+            int j = node.j;
+            if (i - 1 >= 0 && grid[i - 1][j] == 1 && !visited[i - 1][j]) {//top
+                visited[i - 1][j] = true;
+                queue.offer(new Pair(i - 1, j));
+            }
+            if (i + 1 < n && grid[i + 1][j] == 1 && !visited[i + 1][j]) {//bottom
+                visited[i + 1][j] = true;
+                queue.offer(new Pair(i + 1, j));
+            }
+            if (j - 1 >= 0 && grid[i][j - 1] == 1 && !visited[i][j - 1]) {//left
+                visited[i - 1][j] = true;
+                queue.offer(new Pair(i, j - 1));
+            }
+            if (j + 1 < m && grid[i][j + 1] == 1 && !visited[i][j + 1]) {//right
+                visited[i][j + 1] = true;
+                queue.offer(new Pair(i, j + 1));
+            }
+            count++;
         }
-        if (grid[r][c] == 0 || grid[r][c] == 2) {
-            return 0;
-        }
-
-        grid[r][c] = 2;
-
-        int left = dfs(grid, r, c - 1);
-        int right = dfs(grid, r, c + 1);
-        int top = dfs(grid, r - 1, c);
-        int bottom = dfs(grid, r + 1, c);
-
-        return 1 + left + right + top + bottom;
+        return count;
     }
 }

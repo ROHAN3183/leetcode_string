884d5b16a061af50f37329199dbb46b276122233
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-18 16:50:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,34 +1,35 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (grid[r][c] == 1) {
-                    return dfs(grid, r, c);
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    result = dfs(grid, i, j, visited);
                 }
             }
         }
-        return -1;
+        return result;
 
     }
 
-    int dfs(int[][] grid, int r, int c) {
-        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length) {
+    int dfs(int[][] grid, int i, int j, boolean visited[][]) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
             return 1;
         }
-        if (grid[r][c] == 0) {
-            return 1;
-        }
-        if (grid[r][c] == 2) {
+        if (visited[i][j]) {
             return 0;
         }
-        grid[r][c] = 2;
-        int left = dfs(grid, r, c - 1);
-        int right = dfs(grid, r, c + 1);
-        int top = dfs(grid, r - 1, c);
-        int bottom = dfs(grid, r + 1, c);
+        visited[i][j] = true;
+
+        int left = dfs(grid, i, j - 1, visited);
+        int right = dfs(grid, i, j + 1, visited);
+        int top = dfs(grid, i - 1, j, visited);
+        int bottom = dfs(grid, i + 1, j, visited);
+
         return left + right + top + bottom;
+
     }
 }

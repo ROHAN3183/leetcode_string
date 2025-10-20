9f6b702127652927472b9691a19e38ab0862aaa1
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-20 18:10:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,61 +1,62 @@
 class Solution {
     public void solve(char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
-
-        // left to right
-         for (int c = 0; c < n; c++) {
-        if (board[0][c] == 'O') {
-            dfs(board, 0, c);
-        }
-    }
-
-    // bottom row
-    for (int c = 0; c < n; c++) {
-        if (board[m - 1][c] == 'O') {
-            dfs(board, m - 1, c);
-        }
-    }
-
-    // left column
-    for (int r = 0; r < m; r++) {
-        if (board[r][0] == 'O') {
-            dfs(board, r, 0);
-        }
-    }
-
-    // right column
-    for (int r = 0; r < m; r++) {
-        if (board[r][n - 1] == 'O') {
-            dfs(board, r, n - 1);
-        }
-    }
-
-        // final result
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (board[r][c] == 'O') {
-                    board[r][c] = 'X';
-                } else if (board[r][c] == 'B') {
-                    board[r][c] = 'O';
+        int n = board.length;
+        int m = board[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+                    continue;
+                } else if (board[i][j] == 'O' && dfs1(board, i - 1, j) && dfs2(board, i + 1, j)
+                        && dfs3(board, i, j - 1) && dfs4(board, i, j + 1)) {
+                    board[i][j] = 'X';
                 }
             }
         }
+        return;
     }
 
-    void dfs(char[][] board, int r, int c) {
-        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length) {
-            return;
-        }
-        if (board[r][c] != 'O') {
-            return;
-        }
+    boolean dfs1(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length)
+            return false;
+        if (board[i][j] == 'X')
+            return true;
 
-        board[r][c] = 'B';
+        board[i][j] = '#';
+        boolean res = dfs1(board, i - 1, j);
+        board[i][j] = 'O';
+        return res;
+    }
 
-        dfs(board, r + 1, c);
-        dfs(board, r - 1, c);
-        dfs(board, r, c + 1);
-        dfs(board, r, c - 1);
+    boolean dfs2(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length)
+            return false;
+        if (board[i][j] == 'X')
+            return true;
+        board[i][j] = '#';
+        boolean res = dfs2(board, i + 1, j);
+        board[i][j] = 'O';
+        return res;
+    }
+
+    boolean dfs3(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length)
+            return false;
+        if (board[i][j] == 'X')
+            return true;
+        board[i][j] = '#';
+        boolean res = dfs3(board, i, j - 1);
+        board[i][j] = 'O';
+        return res;
+    }
+
+    boolean dfs4(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length)
+            return false;
+        if (board[i][j] == 'X')
+            return true;
+        board[i][j] = '#';
+        boolean res = dfs4(board, i, j + 1);
+        board[i][j] = 'O';
+        return res;
     }
 }

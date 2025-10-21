76025feddf7d197c8680cf540bc1e77fd48bbcb1
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-21 21:19:45 +0530

    Time: 769 ms (9.96%), Space: 45 MB (96.08%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,62 +1,42 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        int n = wordList.size();
-        boolean[] visited = new boolean[n];
-        int res = dfs(wordList, beginWord, endWord, visited);
-        return res == Integer.MAX_VALUE ? 0 : res + 1;
+        Queue<String> queue = new LinkedList<>();
+        return bfs(wordList, beginWord, endWord, queue);
+
     }
 
-    int dfs(List<String> wordList, String beginWord, String endWord, boolean[] visited) {
-        if (isAnswer(beginWord, endWord)) {
-            return 0;
-        }
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < wordList.size(); i++) {
-            if (!visited[i] && isValid(beginWord, wordList.get(i))) {
-                visited[i] = true;
-                int val = dfs(wordList, wordList.get(i), endWord, visited);
-                if (val != Integer.MAX_VALUE) {
-                    min = Math.min(min, 1 + val);
+    int bfs(List<String> wordList, String beginWord, String endWord, Queue<String> queue) {
+        queue.offer(beginWord);
+        boolean[] visited = new boolean[wordList.size()];
+        int step = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int j = 0; j < size; j++) {
+                String curr = queue.poll();
+                if (curr.equals(endWord)) {
+                    return step;
                 }
-                visited[i] = false;
+                for (int i = 0; i < wordList.size(); i++) {
+                    if (!visited[i] && isValid(curr, wordList.get(i))) {
+                        visited[i] = true;
+                        queue.offer(wordList.get(i));
+
+                    }
+                }
             }
+            step++;
         }
-        return min;
+        return 0;
     }
 
     boolean isValid(String prev, String next) {
-        if (prev.length() > next.length() || prev.length() < next.length()) {
+        if (prev.length() != next.length())
             return false;
-        }
-        int i = 0;
-        int j = 0;
         int diff = 0;
-        int n = prev.length();
-        while (i < n) {
-            if (prev.charAt(i) != next.charAt(j)) {
+        for (int i = 0; i < prev.length(); i++) {
+            if (prev.charAt(i) != next.charAt(i))
                 diff++;
-            }
-            i++;
-            j++;
         }
         return diff == 1;
-
-    }
-
-    boolean isAnswer(String beginWord, String endWord) {
-        if (beginWord.length() > endWord.length() || beginWord.length() < endWord.length()) {
-            return false;
-        }
-        int i = 0;
-        int j = 0;
-        int n = beginWord.length();
-        while (i < n && j < n) {
-            if (beginWord.charAt(i) != endWord.charAt(j)) {
-                return false;
-            }
-            i++;
-            j++;
-        }
-        return true;
     }
 }

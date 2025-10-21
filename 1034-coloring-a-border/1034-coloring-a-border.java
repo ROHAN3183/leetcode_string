d7abfd6d53f96c554b80a588a2e42811f0d769cf
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-22 00:49:08 +0530

    Time: 2 ms (30.24%), Space: 45.6 MB (62.57%) - LeetHub

--- 1034-coloring-a-border/1034-coloring-a-border.java
+++ 1034-coloring-a-border/1034-coloring-a-border.java
@@ -13,10 +13,9 @@ class Solution {
         Queue<Pair> queue = new LinkedList<>();
         int n = grid.length;
         int m = grid[0].length;
-        boolean[][] visited = new boolean[n][m];
         int[][] result = new int[n][m];
 
-        bfs(grid, queue, row, col, color, visited, result);
+        bfs(grid, queue, row, col, color, result);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -30,59 +29,59 @@ class Solution {
 
     }
 
-    void bfs(int[][] grid, Queue<Pair> queue, int row, int col, int newColor, boolean[][] visited,int [][] result) {
+    void bfs(int[][] grid, Queue<Pair> queue, int row, int col, int newColor, int[][] result) {
         int n = grid.length;
         int m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
         int targetColor = grid[row][col];
         queue.offer(new Pair(row, col));
         visited[row][col] = true;
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Pair node = queue.poll();
-            int i=node.i;
-            int j=node.j;
+            int i = node.i;
+            int j = node.j;
 
-            boolean left=true;
-            boolean top=true;
-            boolean right=true;
-            boolean bottom=true;
+            boolean left = true;
+            boolean top = true;
+            boolean right = true;
+            boolean bottom = true;
 
-            if((i-1>=0 && grid[i-1][j]!=targetColor)||(i-1<0)){
-                top=!top;
+            if (i - 1 < 0 || grid[i - 1][j] != targetColor) {
+                top = !top;
             }
-            if((i+1<n&& grid[i+1][j]!=targetColor)||(i+1>=n)){
-                bottom=!bottom;
+            if (i + 1 >= n || grid[i + 1][j] != targetColor) {
+                bottom = !bottom;
             }
-            if((j-1>=0 && grid[i][j-1]!=targetColor)||(j-1<0)){
-                left=!left;
+            if (j - 1 < 0 || grid[i][j - 1] != targetColor) {
+                left = !left;
             }
-            if((j+1<m && grid[i][j+1]!=targetColor)||(j+1>=m)){
-                right=!right;
-            }
-            
-            if((grid[i][j]==targetColor)&&(!top||!bottom||!left||!right)){
-                result[i][j]=newColor;
+            if (j + 1 >= m || grid[i][j + 1] != targetColor) {
+                right = !right;
             }
 
-            if(i-1>=0 && grid[i-1][j]==targetColor&&!visited[i-1][j]){
-                visited[i-1][j]=true;
-                queue.offer(new Pair(i-1,j));
+            if (!top || !bottom || !left || !right) {
+                result[i][j] = newColor;
             }
-            if(i+1<n&& grid[i+1][j]==targetColor && !visited[i+1][j]){
-                visited[i+1][j]=true;
-                queue.offer(new Pair(i+1,j));
+
+            if (i - 1 >= 0 && grid[i - 1][j] == targetColor && !visited[i - 1][j]) {
+                visited[i - 1][j] = true;
+                queue.offer(new Pair(i - 1, j));
             }
-            if(j-1>=0 && grid[i][j-1]==targetColor &&!visited[i][j-1]){
-                visited[i][j-1]=true;
-                queue.offer(new Pair(i,j-1));
+            if (i + 1 < n && grid[i + 1][j] == targetColor && !visited[i + 1][j]) {
+                visited[i + 1][j] = true;
+                queue.offer(new Pair(i + 1, j));
             }
-            if(j+1<m && grid[i][j+1]==targetColor && !visited[i][j+1]){
-                visited[i][j+1]=true;
-                queue.offer(new Pair(i,j+1));
+            if (j - 1 >= 0 && grid[i][j - 1] == targetColor && !visited[i][j - 1]) {
+                visited[i][j - 1] = true;
+                queue.offer(new Pair(i, j - 1));
             }
-            
+            if (j + 1 < m && grid[i][j + 1] == targetColor && !visited[i][j + 1]) {
+                visited[i][j + 1] = true;
+                queue.offer(new Pair(i, j + 1));
+            }
+
         }
         return;
-
 
     }
 }

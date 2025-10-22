11ad7dcb156b5ead29d266f2c8ec5541feb78218
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-23 02:44:38 +0530

    Time: 173 ms (5.01%), Space: 45.9 MB (11.01%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int n = heights.length;
+        int m = heights[0].length;
+        int min = 0;
+        int max = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                min = Math.min(min, heights[i][j]);
+                max = Math.max(max, heights[i][j]);
+            }
+        }
+        int low = min;
+        int high = max;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            boolean[][] visited = new boolean[n][m];
+            if (dfs(heights, visited, heights[0][0], 0, 0, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    boolean dfs(int[][] heights, boolean[][] visited, int prev, int i, int j, int target) {
+        int n = heights.length;
+        int m = heights[0].length;
+        if (i >= n || i < 0 || j >= m || j < 0 || Math.abs(prev - heights[i][j]) > target || visited[i][j]) {
+            return false;
+        }
+        if (i == n - 1 && j == m - 1) {
+            return true;
+        }
+        visited[i][j] = true;
+        boolean left = dfs(heights, visited, heights[i][j], i, j - 1, target);
+        boolean right = dfs(heights, visited, heights[i][j], i, j + 1, target);
+        boolean top = dfs(heights, visited, heights[i][j], i - 1, j, target);
+        boolean bottom = dfs(heights, visited, heights[i][j], i + 1, j, target);
+
+        return left || right || top || bottom;
+
+    }
+}

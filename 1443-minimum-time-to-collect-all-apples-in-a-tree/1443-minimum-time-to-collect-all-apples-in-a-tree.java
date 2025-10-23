1ce2a59f74ca9799e76b17641e39793da590d7c5
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-23 21:38:12 +0530

    Time: 19 ms (74.53%), Space: 75.9 MB (95.93%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
@@ -1,51 +1,32 @@
 class Solution {
     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        HashMap<Integer, Boolean> map = new HashMap<>();
         boolean[] visited = new boolean[n];
         int count = 0;
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
-            map.put(i, false);
         }
         for (int[] e : edges) {
             adj.get(e[0]).add(e[1]);
             adj.get(e[1]).add(e[0]);
         }
 
-        dfs(adj, 0, visited, hasApple, map);
-        for (boolean val : map.values()) {
-            if (val) {
-                count++;
-            }
-        }
-        if (count * 2 - 2 <= 0) {
-            return 0;
-        }
-        return count * 2 - 2;
+        return dfs(adj, 0, visited, hasApple);
+
     }
 
-    void dfs(ArrayList<ArrayList<Integer>> adj, int i, boolean[] visited, List<Boolean> hasApple,
-            HashMap<Integer, Boolean> map) {
+    int dfs(ArrayList<ArrayList<Integer>> adj, int i, boolean[] visited, List<Boolean> hasApple) {
         visited[i] = true;
+        int time = 0;
         for (int neigbour : adj.get(i)) {
             if (!visited[neigbour]) {
-                dfs(adj, neigbour, visited, hasApple, map);
-            }
-        }
-        if (hasApple.get(i)) {
-            map.put(i, true);
-        } else {
-            boolean hasAppleInNeighbor = false;
-            for (int neighbor : adj.get(i)) {
-                if (map.get(neighbor)) {
-                    hasAppleInNeighbor = true;
-                    break;
+                int childTime = dfs(adj, neigbour, visited, hasApple);
+                if (childTime > 0 || hasApple.get(neigbour)) {
+                    time += childTime + 2;
                 }
             }
-            map.put(i, hasAppleInNeighbor);
-
         }
-        return;
+
+        return time;
     }
 }

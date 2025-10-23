49b13ead898810657aa0f8e0aacdf8b57f981869
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-23 20:50:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+            map.put(i, false);
+        }
+        for (int[] e : edges) {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+
+        dfs(adj, 0, visited, hasApple, map);
+        for (boolean val : map.values()) {
+            if (val) {
+                count++;
+            }
+        }
+        if (count*2 - 2 <= 0) {
+            return 0;
+        }
+        return count*2 ;
+    }
+
+    void dfs(ArrayList<ArrayList<Integer>> adj, int i, boolean[] visited, List<Boolean> hasApple,
+            HashMap<Integer, Boolean> map) {
+        visited[i] = true;
+        for (int neigbour : adj.get(i)) {
+            if (!visited[neigbour]) {
+                dfs(adj, neigbour, visited, hasApple, map);
+            }
+        }
+        if (hasApple.get(i)) {
+            map.put(i, true);
+        } else if (adj.get(i).size() == 1) {
+            if (map.get(adj.get(i).get(0))) {
+                map.put(i, true);
+            } else {
+                map.put(i, false);
+            }
+        } else if (adj.get(i).size() == 2) {
+            if (map.get(adj.get(i).get(0)) || map.get(adj.get(i).get(1))) {
+                map.put(i, true);
+            } else {
+                map.put(i, false);
+            }
+        } else {
+            map.put(i, false);
+        }
+        return;
+    }
+}

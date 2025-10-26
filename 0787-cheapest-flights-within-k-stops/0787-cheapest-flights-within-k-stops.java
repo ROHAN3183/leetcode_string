c05d6d995886c223a83d27700d178196e92f1479
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-26 14:47:42 +0530

    Time: 6 ms (81.91%), Space: 44.5 MB (59.04%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,69 +1,48 @@
 class Solution {
     class Pair {
-        int node0;
-        int cost0;
-
-        Pair(int node0, int cost0) {
-            this.node0 = node0;
-            this.cost0 = cost0;
-
+        int node, cost;
+        Pair(int node, int cost) {
+            this.node = node;
+            this.cost = cost;
         }
     }
 
     class Info {
-        int node1;
-        int cost1;
-        int step;
-
-        Info(int node1, int step, int cost1) {
-            this.node1 = node1;
-            this.step = step;
-            this.cost1 = cost1;
-
+        int node, cost, stops;
+        Info(int node, int stops, int cost) {
+            this.node = node;
+            this.stops = stops;
+            this.cost = cost;
         }
     }
 
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] distance = new int[n];
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
-        }
-        for (int[] edge : flights) {
-            int u = edge[0];
-            int v = edge[1];
-            int w = edge[2];
-            adj.get(u).add(new Pair(v, w));
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for (int[] edge : flights)
+            adj.get(edge[0]).add(new Pair(edge[1], edge[2]));
 
-        }
-        return bfs(adj, src, dst, k);
+        Queue<Info> q = new LinkedList<>();
+        q.offer(new Info(src, 0, 0));
 
-    }
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
 
-    int bfs(ArrayList<ArrayList<Pair>> adj, int src, int dst, int k) {
-        Queue<Info> queue = new LinkedList<>();
-        int [] distance = new int [adj.size()];
-        Arrays.fill(distance,Integer.MAX_VALUE);
-        queue.offer(new Info(src, -1, 0));
-        distance[src]=0;
-        int min = Integer.MAX_VALUE;
-        while (!queue.isEmpty()) {
-            Info X = queue.poll();
-            if (X.node1 == dst && X.step <= k) {
-                min = Math.min(min, X.cost1);
-                continue;
-            }
-            if (X.step >= k) {
-                continue;
-            }
-            for (Pair neibour : adj.get(X.node1)) {
-                if(X.cost1+neibour.cost0<distance[neibour.node0]){
-                    distance[neibour.node0]=X.cost1+neibour.cost0;
-                    queue.offer(new Info(neibour.node0, X.step + 1, X.cost1 + neibour.cost0));
+        while (!q.isEmpty()) {
+            Info cur = q.poll();
+
+            if (cur.stops > k) continue;
+
+            for (Pair nei : adj.get(cur.node)) {
+                int newCost = cur.cost + nei.cost;
+                if (newCost < dist[nei.node] && cur.stops <= k) {
+                    dist[nei.node] = newCost;
+                    q.offer(new Info(nei.node, cur.stops + 1, newCost));
                 }
             }
         }
-        return min == Integer.MAX_VALUE ? -1 : min;
-    }
 
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
 }

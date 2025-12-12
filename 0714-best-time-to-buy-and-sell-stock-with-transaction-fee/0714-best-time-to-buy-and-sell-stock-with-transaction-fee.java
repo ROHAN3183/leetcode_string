99255bd5f9303a5c5a7f2cf53626f18af24ee851
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-12-12 14:57:07 +0530

    Time: 46 ms (5.36%), Space: 98.3 MB (5.11%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,29 +1,30 @@
 class Solution {
-    Integer [][] dp;
+    Integer[][] dp;
+
     public int maxProfit(int[] prices, int fee) {
-        dp = new Integer [prices.length][2];
-        return dfs(prices,fee,0,-1);
+        dp = new Integer[prices.length][2];
+        return dfs(prices, fee, 0, 0);
     }
-    int dfs(int [] prices , int fee , int idx , int buy){
-        if(idx>=prices.length){
+
+    int dfs(int[] prices, int fee, int idx, int holding) {
+        if (idx >= prices.length) {
             return 0;
         }
-        int buyState = (buy==-1?0:1);
-        if(dp[idx][buyState]!=null){
-            return dp[idx][buyState];
+
+        if (dp[idx][holding] != null) {
+            return dp[idx][holding];
         }
         int ans;
 
-        if(buy==-1){
-            int take  = -prices[idx]+dfs(prices, fee , idx+1, prices[idx]);
-            int nottake = dfs(prices,fee , idx+1,buy);
-            ans=Math.max(take,nottake);
+        if (holding == 0) {
+            int take = -prices[idx] + dfs(prices, fee, idx + 1, 1);
+            int nottake = dfs(prices, fee, idx + 1, holding);
+            ans = Math.max(take, nottake);
+        } else {
+            int sell = (prices[idx] - fee) + dfs(prices, fee, idx + 1, 0);
+            int notsell = dfs(prices, fee, idx + 1, holding);
+            ans = Math.max(sell, notsell);
         }
-        else{
-            int sell = (prices[idx] - fee )+ dfs(prices,fee,idx+1,-1);
-            int notsell = dfs(prices,fee , idx+1,buy);
-            ans= Math.max(sell,notsell);
-        }
-        return dp[idx][buyState]=ans;
+        return dp[idx][holding] = ans;
     }
 }

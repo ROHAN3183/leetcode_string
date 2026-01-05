5849ba9f1d3bd9640392802aab82ff9567c16e41
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-01-05 18:10:56 +0530

    Time: 8 ms (26.33%), Space: 67 MB (15.09%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
@@ -1,23 +1,30 @@
-logic of the code:
-use Kadane’s algorithm check wheather comming number is greater then the sum of the previous then make it as then the max_sum 
-find both the max_sum of the subarray also find the min_sum of the subarray because we have to find the absolute differnce of the
-subarray.  
 class Solution {
     public int maxAbsoluteSum(int[] nums) {
-        int n=nums.length;
-        int current=nums[0];
-        int max_sum=nums[0];
-        for(int i=1;i<n;i++){
-            current=Math.max(nums[i],current+nums[i]);
-            max_sum=Math.max(current,max_sum);
+
+        int max = maximumSubarraySum(nums);
+        int min = minimumSubarraySum(nums);
+
+        return Math.max(Math.abs(max), Math.abs(min));
+
+    }
+
+    int maximumSubarraySum(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int currSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currSum = Math.max(currSum + nums[i], nums[i]);
+            max = Math.max(max, currSum);
         }
-        current=nums[0];
-       int  min_sum=nums[0];
-        for(int i=1;i<n;i++){
-            current=Math.min(nums[i],current+nums[i]);
-            min_sum=Math.min(current,min_sum);
+        return max;
+    }
+
+    int minimumSubarraySum(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int currSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currSum = Math.min(currSum + nums[i], nums[i]);
+            min = Math.min(min, currSum);
         }
-        int result=Math.max(Math.abs(max_sum),Math.abs(min_sum));
-        return result;
+        return min;
     }
 }

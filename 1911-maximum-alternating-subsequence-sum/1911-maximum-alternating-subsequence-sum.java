aff471cb530d6c135ec7aabb5e4b5ce413b31beb
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-01-08 18:27:51 +0530

    Time: 68 ms (17.34%), Space: 165.6 MB (5.14%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    Long[][] dp;
+
+    public long maxAlternatingSum(int[] nums) {
+        dp = new Long[nums.length][2];
+        return dfs(nums, 0, 1);
+    }
+
+    long dfs(int[] nums, int idx, int flag) {
+        if (idx >= nums.length) {
+            return 0;
+        }
+        if (dp[idx][flag] != null) {
+            return dp[idx][flag];
+        }
+        long option1 = 0;
+        long option2 = 0;
+        if (flag == 1) {
+            long take = nums[idx] + dfs(nums, idx + 1, 0);
+            long nottake = dfs(nums, idx + 1, 1);
+            option1 = Math.max(take, nottake);
+        } else {
+            long take = -nums[idx] + dfs(nums, idx + 1, 1);
+            long nottake = dfs(nums, idx + 1, 0);
+            option2 = Math.max(take, nottake);
+        }
+        return dp[idx][flag] = Math.max(option1, option2);
+    }
+}

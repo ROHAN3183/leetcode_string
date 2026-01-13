5e67365621fb5b1c5d31028fdd07f1b8f31b20a4
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-01-13 20:49:34 +0530

    Time: 29 ms (34.4%), Space: 43.3 MB (47.2%) - LeetHub

--- 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.java
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.java
@@ -1,29 +1,25 @@
 class Solution {
-    int max = 0;
-
     public int maxCompatibilitySum(int[][] students, int[][] mentors) {
         boolean[] used = new boolean[mentors.length];
-        dfs(students, mentors, used, 0, 0);
-        return max;
+        return dfs(students, mentors, used, 0);
     }
 
-    void dfs(int[][] students, int[][] mentors, boolean[] used, int idx, int ans) {
+    int dfs(int[][] students, int[][] mentors, boolean[] used, int idx) {
         if (idx == students.length) {
-            max = Math.max(max, ans);
-            return;
+            return 0;
         }
-
         int[] s = students[idx];
+        int best = 0;
         for (int i = 0; i < mentors.length; i++) {
             int[] m = mentors[i];
             if (!used[i]) {
                 used[i] = true;
-                ans += check(s, m);
-                dfs(students, mentors, used, idx + 1, ans);
-                ans -= check(s, m);
+                int score = check(s, m) + dfs(students, mentors, used, idx + 1);
+                best = Math.max(score, best);
                 used[i] = false;
             }
         }
+        return best;
     }
 
     int check(int[] s, int[] m) {

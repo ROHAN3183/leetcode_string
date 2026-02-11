079a2d9e89cf91dbb2b04d11b0c09ebbed62acb4
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-02-11 11:58:12 +0530

    Time: 1 ms (72.11%), Space: 44 MB (13.21%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,20 +1,33 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer>result=new ArrayList<>();
-        sideView(root,result,1);
-        return result;
-        
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        return bfs(root);
     }
-    void sideView(TreeNode root,List<Integer>result,int level){
-        if(root==null){
-            return;
-        }
-        if(result.size()<level){
-            result.add(root.val);
+
+    List<Integer> bfs(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            TreeNode ans = null;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                ans = node;
+            }
+            result.add(ans.val);
 
         }
-        sideView(root.right,result,level+1);
-        sideView(root.left,result,level+1);
-
+        return result;
     }
 }

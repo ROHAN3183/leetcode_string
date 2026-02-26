eb48679e2e7dfadcfa6665795dfbc1ac581a8319
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-02-26 20:29:50 +0530

    Time: 289 ms (10.17%), Space: 46.4 MB (96.82%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,52 +1,13 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-# brute force
 public class Solution {
-    public ListNode detectCycle(ListNode head) { //In hashMap if it contain's node it means loop do exits. 
-        HashMap<ListNode,Integer>map=new HashMap<>();
-        ListNode temp=head;
-        while(temp!=null){
-            if(map.containsKey(temp)){
+    public ListNode detectCycle(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        ListNode temp = head;
+        while (temp != null) {
+            if (list.contains(temp)) {
                 return temp;
             }
-            map.put(temp,1);
-            temp=temp.next;
-        }
-    return null;
-    }
-}
-
-# optimized code
-
-public class Solution {
-    public ListNode detectCycle(ListNode head) { //use slow and fast approach if slow =fast it means loop do exist's.
-        ListNode slow=head;
-        ListNode fast=head;
-        boolean flag=false;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-              if(slow==fast){
-                flag=true;
-                break;
-            }
-        }
-        ListNode temp=head;
-        if(flag){
-            while(temp!=slow){
-                temp=temp.next;
-                slow=slow.next;
-            }
-            return slow;
+            list.add(temp);
+            temp = temp.next;
         }
         return null;
     }

6d4b1a23a31f49178cbf6494cbe6b2eea03636f3
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-02-28 08:16:05 +0530

    Time: 0 ms (100%), Space: 46.3 MB (90.19%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,28 +1,21 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode oddEvenList(ListNode head) { // Assume first node as odd and second node as even.
-        if(head==null){
-            return null;
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
         }
-        ListNode odd=head;
-        ListNode even=head.next;
-        ListNode start_even=even;
-        while(even!=null && even.next!= null){
-            odd.next=even.next;
-            even.next=even.next.next;
-            odd=odd.next;
-            even=even.next;
+        ListNode even = head;
+        ListNode odd = head.next;
+        ListNode temp = odd;
+        while (odd != null && odd.next != null) {
+            even.next = odd.next;
+            even = even.next;
+            odd.next = even.next;
+            odd = odd.next;
         }
-        odd.next=head.next;
+        if (odd != null) {
+            odd.next = null;
+        }
+        even.next = temp;
         return head;
     }
 }

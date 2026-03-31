0a70ee66d4e2837590ddf7b0e9e87f27fa9a1fc8
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-03-31 18:35:30 +0530

    Time: 0 ms (100%), Space: 43 MB (12.26%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if (a.length() != b.length()) {
+            return Math.max(a.length(), b.length());
+        } else if (!a.equals(b)) {
+            return a.length();
+        }
+        return -1;
+    }
+}

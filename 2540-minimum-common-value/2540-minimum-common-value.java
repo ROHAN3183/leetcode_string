07aa82b52647d4f1e4f41dd58f4e0d280de946e2
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-05-19 18:47:03 +0530

    Time: 7 ms (26.7%), Space: 80.5 MB (35.01%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,17 +1,26 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        int i = 0;
-        int j = 0;
-        int min = Integer.MAX_VALUE;
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] == nums2[j]) {
-                return nums1[i];
-            } else if (nums1[i] < nums2[j]) {
-                i++;
-            } else if (nums2[j] < nums1[i]) {
-                j++;
+        for (int k = 0; k < nums1.length; k++) {
+            if (isvalidBS(nums1[k], nums2)) {
+                return nums1[k];
             }
         }
         return -1;
+    }
+
+    boolean isvalidBS(int target, int[] nums2) {
+        int low = 0;
+        int high = nums2.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums2[mid] == target) {
+                return true;
+            } else if (nums2[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
     }
 }

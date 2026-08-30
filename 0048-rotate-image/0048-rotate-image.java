ef8536e9f13047c97d04b2b54adf66c7ad8a4749
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-08-31 01:42:40 +0530

    Time: 0 ms (100%), Space: 43.7 MB (53.16%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            reverse(matrix[i]);
+        }
+        return;
+    }
+
+    void reverse(int[] matrix) {
+        int i = 0;
+        int j = matrix.length - 1;
+        while (i < j) {
+            int temp = matrix[i];
+            matrix[i] = matrix[j];
+            matrix[j] = temp;
+            i++;
+            j--;
+        }
+        return;
+    }
+}

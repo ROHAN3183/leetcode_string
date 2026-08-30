8a555f18c47522e50ad187362b44af34ff9916ab
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-08-30 12:45:56 +0530

    Time: 0 ms (100%), Space: 43.6 MB (78.69%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int idx = nums1.length - 1;
+        int i = m - 1;
+        int j = n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums2[j] >= nums1[i]) {
+                nums1[idx] = nums2[j];
+                j--;
+            } else if (nums1[i] >= nums2[j]) {
+                nums1[idx] = nums1[i];
+                i--;
+            }
+            idx--;
+        }
+        while (i >= 0) {
+            nums1[idx] = nums1[i];
+            i--;
+            idx--;
+        }
+        while (j >= 0) {
+            nums1[idx] = nums2[j];
+            j--;
+            idx--;
+        }
+        return;
+
+    }
+}

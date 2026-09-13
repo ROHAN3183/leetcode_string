3787db841719c5c97e15047ba4c172c80ccd091f
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-09-13 18:50:35 +0530

    Time: 9 ms (96.19%), Space: 59.4 MB (48.38%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,18 +1,51 @@
-
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer>list=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            list.add(temp.val);
-            temp=temp.next;
+        if (head == null || head.next == null) {
+            return head;
         }
-        Collections.sort(list);
-        temp=head;
-        for(int i=0;i<list.size();i++){
-            temp.val=list.get(i);
-            temp=temp.next;
-        }
-        return head;
+        return mergeSort(head);
     }
+
+    ListNode mergeSort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode l1 = mergeSort(head);
+        ListNode l2 = mergeSort(slow);
+        return merge(l1, l2);
+
+    }
+
+    ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                curr.next = l2;
+                l2 = l2.next;
+            } else {
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            curr = curr.next;
+        }
+        if (l1 != null) {
+            curr.next = l1;
+        }
+        if (l2 != null) {
+            curr.next = l2;
+
+        }
+        return dummy.next;
+    }
+
 }

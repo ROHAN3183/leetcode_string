bce8965bbea6409048fb7e0adee6c8cb53a72ffe
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-09-14 09:18:47 +0530

    Time: 0 ms (100%), Space: 44.4 MB (44.35%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,41 +1,30 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode rotateRight(ListNode head, int k) { //calculate the length of linklist and also maintain tail for last node
-        ListNode temp=head;                            //add the tail.next =head.
-        ListNode tail=head;                            // get the length where need to put null using subtracting length of lini
-        int length=0;                                  //with the position to be change
-        while(temp!=null){
-            length++;
-            tail=temp;
-            temp=temp.next;
-        }
-        if(k==0){
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) {
             return head;
         }
-        if(head==null){
-            return null;
+        ListNode temp = head;
+        int n = 0;
+        ListNode tail = null;
+        while (temp != null) {
+            n++;
+            tail = temp;
+            temp = temp.next;
         }
-        k=k%length;
-
-        tail.next=head;
-        ListNode temp2=head;
-        int pos=length-k;
-        int result=0;
-        while(result<pos-1){
-            result++;
-            temp2=temp2.next;
+        k = k % n;
+        if (k <= 0) {
+            return head;
         }
-        ListNode ans=temp2.next;
-        temp2.next=null;
-        return ans;
+        int pos = n - k;
+        temp = head;
+        ListNode prev = null;
+        while (pos > 0) {
+            pos--;
+            prev = temp;
+            temp = temp.next;
+        }
+        prev.next = null;
+        tail.next = head;
+        return temp;
     }
 }

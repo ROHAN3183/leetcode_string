1f2b759f522751b98578d0a7b8d4dc716e33cc90
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-09-14 23:22:06 +0530

    Time: 2 ms (97.45%), Space: 72.4 MB (49.34%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        int twenty = 0;
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5) {
+                five++;
+            } else if (bills[i] == 10) {
+                if (five < 1) {
+                    return false;
+                }
+                five--;
+                ten++;
+            } else {
+
+                if (five >= 1 && ten >= 1) {
+                    five = five - 1;
+                    ten = ten - 1;
+                    twenty++;
+                } else if (five >= 3) {
+                    five = five - 3;
+                    twenty++;
+
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}

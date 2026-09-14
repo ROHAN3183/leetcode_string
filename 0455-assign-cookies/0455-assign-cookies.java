90d3e2e9c107cc9081bbf99281ee4c7cf3046965
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-09-14 21:38:50 +0530

    Time: 16 ms (10.2%), Space: 51.7 MB (19.26%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,24 +1,18 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-
-        int no_children=g.length;
-        int no_cookies= s.length;
-
         Arrays.sort(g);
         Arrays.sort(s);
-        int count=0;
-        int i=0;
-        int j=0;
-
-        while(i<no_children && j<no_cookies){
-
-            if(g[i]<=s[j]){
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
                 count++;
                 i++;
+                j++;
+            } else if (g[i] > s[j]) {
+                j++;
             }
-
-            j++;
-
         }
         return count;
     }

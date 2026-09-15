106b5eefaf80abc42e47eda3bc3ed5aa5ebad699
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-09-15 19:08:55 +0530

    Time: 350 ms (6.02%), Space: 48.1 MB (8.63%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,27 @@
+class Solution {
+    Boolean[] dp;
+
+    public boolean canJump(int[] nums) {
+        dp = new Boolean[nums.length];
+        return dfs(nums, 0);
+    }
+
+    boolean dfs(int[] nums, int idx) {
+        if (idx >= nums.length - 1) {
+            return true;
+        }
+        if (nums[idx] == 0) {
+            return false;
+        }
+        if (dp[idx] != null) {
+            return dp[idx];
+        }
+        for (int i = 1; i <= nums[idx]; i++) {
+            if (dfs(nums, idx + i)) {
+                return true;
+            }
+        }
+        dp[idx] = false;
+        return false;
+    }
+}

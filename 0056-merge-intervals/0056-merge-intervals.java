4367927ad8eded8ae0462024b1532b404fa3ba61
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-09-16 13:47:45 +0530

    Time: 127 ms (5.5%), Space: 48.9 MB (85.1%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,33 +1,24 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        int n=intervals.length;
-        List<int[]>list=new ArrayList<>();
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        int currentStart=intervals[0][0];
-        int currentEnd=intervals[0][1];
+    public int[][] merge(int[][] arr) {
+        ArrayList<int[]> list = new ArrayList<>();
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        int[] interval = new int[] { arr[0][0], arr[0][1] };
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i][0] <= interval[1] && interval[0] <= arr[i][1]) {
+                int min_start = Math.min(arr[i][0], interval[0]);
+                int max_end = Math.max(arr[i][1], interval[1]);
+                interval[0] = min_start;
+                interval[1] = max_end;
 
-        for(int i=1;i<n;i++){
-            
-            int nextStart=intervals[i][0];
-            int nextEnd=intervals[i][1];
-
-            if(currentEnd < nextStart){//non overlapping
-                list.add(new int[]{currentStart,currentEnd});
-                currentStart=nextStart;
-                currentEnd=nextEnd;
-
-            }
-            else if(currentStart <= nextEnd){//overlapping condition
-
-                currentStart=Math.min(currentStart,nextStart);
-                currentEnd=Math.max(currentEnd,nextEnd);
+            } else {
+                list.add(new int[] { interval[0], interval[1] });
+                interval[0] = arr[i][0];
+                interval[1] = arr[i][1];
 
             }
-
         }
-        list.add(new int[]{currentStart,currentEnd});
+        list.add(new int[] { interval[0], interval[1] });
 
-        return list.toArray( new int[list.size()][]);
-        
+        return list.toArray(new int[list.size()][]);
     }
 }

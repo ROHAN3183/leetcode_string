901652c08ea350234be73adee7c0edd44563ccc7
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-09-18 09:57:51 +0530

    Time: 1 ms (99.98%), Space: 50.8 MB (78.4%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -4,24 +4,22 @@ class Solution {
     }
 
     int atMost(int[] nums, int goal) {
-        if (goal < 0)
+        if (goal < 0) {
             return 0;
-
+        }
         int i = 0;
+        int j = 0;
         int sum = 0;
-        int count = 0;
-
-        for (int j = 0; j < nums.length; j++) {
+        int maxCount = 0;
+        while (j < nums.length) {
             sum += nums[j];
-
             while (sum > goal) {
                 sum -= nums[i];
                 i++;
             }
-
-            count += j - i + 1;
+            maxCount += j - i + 1;
+            j++;
         }
-
-        return count;
+        return maxCount;
     }
 }

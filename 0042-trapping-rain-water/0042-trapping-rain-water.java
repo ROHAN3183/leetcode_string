c28bb41662d984875dfdf03583efb2563a08b47c
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2026-09-19 18:24:24 +0530

    Time: 1 ms (56.02%), Space: 48.5 MB (26.36%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,20 +1,22 @@
 class Solution {
     public int trap(int[] height) {
-        int l = 0;
-        int r = height.length - 1;
-        int l_max = 0;
-        int r_max = 0;
+        int l_max = height[0];
+        int r_max = height[height.length - 1];
+        int[] l_prefix = new int[height.length];
+        int[] r_prefix = new int[height.length];
+        l_prefix[0] = l_max;
+        r_prefix[height.length - 1] = r_max;
         int ans = 0;
-        while (l < r) {
-            l_max = Math.max(l_max, height[l]);
-            r_max = Math.max(r_max, height[r]);
-            if (l_max < r_max) {
-                ans += l_max - height[l];
-                l++;
-            } else {
-                ans += r_max - height[r];
-                r--;
-            }
+        for (int i = 1; i < height.length; i++) {
+            l_prefix[i] = Math.max(l_max, height[i]);
+            l_max = l_prefix[i];
+        }
+        for (int i = height.length - 2; i >= 0; i--) {
+            r_prefix[i] = Math.max(r_max, height[i]);
+            r_max = r_prefix[i];
+        }
+        for (int i = 0; i < height.length; i++) {
+            ans += Math.min(l_prefix[i], r_prefix[i]) - height[i];
         }
         return ans;
     }
